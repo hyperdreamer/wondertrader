a8c1f553399a6f99e11deeeb04fece5aa8e170d7
hyperdreamer <wenhepku@gmail.com>
2023-10-12 15:07:25 +0800

    [fix] TimeUtils::makeTime()
    test if mktime() failed or not!

--- src/Share/TimeUtils.hpp
+++ src/Share/TimeUtils.hpp
@@ -212,9 +212,9 @@ public:
         int millisec = lTimeWithMs%1000;
         //t.tm_isdst 	
         time_t ts = mktime(&t);
+        if (ts == -1) return 0; // test if maktime failed or not
         //如果要转成UTC时间，则需要根据时区进行转换
         if (isToUTC) ts -= getTZOffset() * 3600;
-        if (ts == -1) return 0;
         return ts*1000 + millisec;
     }
 
