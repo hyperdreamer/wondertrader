4db537c9eea108841aa95c9887d0cac8d2c8ff3d
hyperdreamer <wenhepku@gmail.com>
2023-11-21 18:21:29 +0800

    [style] BoostMappingFile.hpp/WtDataReader.cpp: brace-style and indentation reflow

--- src/Share/BoostMappingFile.hpp
+++ src/Share/BoostMappingFile.hpp
@@ -12,89 +12,73 @@
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 
-class BoostMappingFile
-{
+class BoostMappingFile {
 public:
-	BoostMappingFile()
-	{
-		_file_map=NULL;
-		_map_region=NULL;
-	}
+    BoostMappingFile()
+    {
+        _file_map = NULL;
+        _map_region = NULL;
+    }
 
-	~BoostMappingFile()
-	{
-		close();
-	}
+    ~BoostMappingFile()
+    {
+        close();
+    }
 
-	void close()
-	{
-		if(_map_region!=NULL)
-			delete _map_region;
+    void close()
+    {
+        if (_map_region) delete _map_region;
+     
+        if (_file_map) delete _file_map;
+     
+        _file_map = NULL;
+        _map_region = NULL;
+    }
 
-		if(_file_map!=NULL)
-			delete _file_map;
+    void sync()
+    {
+        if (_map_region) _map_region->flush();
+    }
 
-		_file_map=NULL;
-		_map_region=NULL;
-	}
+    void* addr()
+    {
+        if (_map_region) return _map_region->get_address();
+        return NULL;
+    }
 
-	void sync()
-	{
-		if(_map_region)
-			_map_region->flush();
-	}
+    size_t size()
+    {
+        if (_map_region) return _map_region->get_size();
+        return 0;
+    }
 
-	void *addr()
-	{
-		if(_map_region)
-			return _map_region->get_address();
-		return NULL;
-	}
+    bool map(const char *filename,
+             int mode = boost::interprocess::read_write,
+             int mapmode = boost::interprocess::read_write,
+             bool zeroother = true)
+    {
+        if (!boost::filesystem::exists(filename)) return false;
+        _file_name = filename;
+     
+        _file_map = new boost::interprocess::file_mapping(filename, (boost::interprocess::mode_t) mode);
+        if (_file_map == NULL) return false;
+     
+        _map_region = new boost::interprocess::mapped_region(*_file_map, (boost::interprocess::mode_t) mapmode);
+        if (_map_region == NULL) {
+            delete _file_map;
+            return false;
+        }
+     
+        return true;
+    }
 
-	size_t size()
-	{
-		if(_map_region)
-			return _map_region->get_size();
-		return 0;
-	}
+    const char* filename() { return _file_name.c_str(); }
 
-	bool map(const char *filename,
-		int mode=boost::interprocess::read_write,
-		int mapmode=boost::interprocess::read_write,bool zeroother=true)
-	{
-		if (!boost::filesystem::exists(filename))
-		{
-			return false;
-		}
-		_file_name = filename;
-
-		_file_map = new boost::interprocess::file_mapping(filename,(boost::interprocess::mode_t)mode);
-		if(_file_map==NULL)
-			return false;
-
-		_map_region = new boost::interprocess::mapped_region(*_file_map,(boost::interprocess::mode_t)mapmode);
-		if(_map_region==NULL)
-		{
-			delete _file_map;
-			return false;
-		}
-
-		return true;
-	}
-
-	const char* filename()
-	{
-		return _file_name.c_str();
-	}
-
-	bool valid() const
-	{
-		return _file_map != NULL;
-	}
+    bool valid() const { return _file_map != NULL; }
 
 private:
-	std::string _file_name;
-	boost::interprocess::file_mapping *_file_map;
-	boost::interprocess::mapped_region *_map_region;
+    std::string _file_name;
+    boost::interprocess::file_mapping* _file_map;
+    boost::interprocess::mapped_region* _map_region;
 };
 
