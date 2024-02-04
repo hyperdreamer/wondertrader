81815c194197be1916e0d86216c228a4d64e9f12
hyperdreamer <wenhepku@gmail.com>
2024-02-04 11:57:51 +0800

    [trivial]

--- src/WtCore/ParserAdapter.cpp
+++ src/WtCore/ParserAdapter.cpp
@@ -44,44 +44,40 @@ ParserAdapter::~ParserAdapter()
 /*
  * @stub: points to the engine, since WtEngine is a child class IParserStub
  */
-bool ParserAdapter::initExt(const char* id, IParserApi* api, IParserStub* stub, IBaseDataMgr* bgMgr, IHotMgr* hotMgr/* = NULL*/)
+bool ParserAdapter::initExt(const char* id, IParserApi* api, IParserStub* stub, IBaseDataMgr* bgMgr, 
+                            IHotMgr* hotMgr/* = NULL*/)
 {
-	if (api == NULL)
-		return false;
+    if (api == NULL) return false;
 
-	_parser_api = api;
-	_stub = stub;
-	_bd_mgr = bgMgr;
-	_hot_mgr = hotMgr;
-	_id = id;
+    _parser_api = api;
+    _stub = stub;
+    _bd_mgr = bgMgr;
+    _hot_mgr = hotMgr;
+    _id = id;
 
-	if (_parser_api)
-	{
-		_parser_api->registerSpi(this);
+    if (_parser_api) {
+        _parser_api->registerSpi(this);
+     
+        if (_parser_api->init(NULL)) {
+            ContractSet contractSet;
+            WTSArray* ayContract = _bd_mgr->getContracts();
+            
+            for (WTSArray::Iterator it = ayContract->begin(); it != ayContract->end(); ++it) {
+                WTSContractInfo* contract = STATIC_CONVERT(*it, WTSContractInfo*);
+                contractSet.insert(contract->getFullCode());
+            }
+         
+            ayContract->release();
+            
+            _parser_api->subscribe(contractSet);
+            contractSet.clear();
+        }
+        else
+            WTSLogger::log_dyn("parser", _id.c_str(), LL_ERROR, 
+                               "[{}] Parser initializing failed: api initializing failed...", _id.c_str());
+    }
 
-		if (_parser_api->init(NULL))
-		{
-			ContractSet contractSet;
-			WTSArray* ayContract = _bd_mgr->getContracts();
-			WTSArray::Iterator it = ayContract->begin();
-			for (; it != ayContract->end(); it++)
-			{
-				WTSContractInfo* contract = STATIC_CONVERT(*it, WTSContractInfo*);
-				contractSet.insert(contract->getFullCode());
-			}
-
-			ayContract->release();
-
-			_parser_api->subscribe(contractSet);
-			contractSet.clear();
-		}
-		else
-		{
-			WTSLogger::log_dyn("parser", _id.c_str(), LL_ERROR, "[{}] Parser initializing failed: api initializing failed...", _id.c_str());
-		}
-	}
-
-	return true;
+    return true;
 }
 
 /*
