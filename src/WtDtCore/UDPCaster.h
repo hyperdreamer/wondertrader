7908c86370050ce828fcfe1047742fd949764f03
hyperdreamer <wenhepku@gmail.com>
2024-05-16 20:36:21 +0800

    [trivial]

--- src/WtDtCore/UDPCaster.h
+++ src/WtDtCore/UDPCaster.h
@@ -53,7 +53,7 @@ private:
     void handle_send_multi(const EndPoint& ep, const boost::system::error_code& error, std::size_t bytes_transferred); 
 
     void do_receive();
-    void do_send();
+    //void do_send();
 
     void broadcast_loop(); // the loop for thread
     void broadcast(WTSObject* data, uint32_t dataType);
