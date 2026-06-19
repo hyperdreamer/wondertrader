3ed97c94d020e674602109b7b46992740bbb0ddd
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:03 +0800

    [style] reformat WtExeFact/WtExeFact.cpp (clang-format: brace style, tabs->spaces)

--- src/WtExeFact/WtExeFact.cpp
+++ src/WtExeFact/WtExeFact.cpp
@@ -4,7 +4,7 @@
  * \author Wesley
  * \date 2020/03/30
  *
- * 
+ *
  */
 #include "WtExeFact.h"
 
@@ -16,26 +16,23 @@
 #include "WtStockVWapExeUnit.h"
 const char* FACT_NAME = "WtExeFact";
 
-extern "C"
+extern "C" {
+EXPORT_FLAG IExecuterFact* createExecFact()
 {
-	EXPORT_FLAG IExecuterFact* createExecFact()
-	{
-		IExecuterFact* fact = new WtExeFact();
-		return fact;
-	}
+    IExecuterFact* fact = new WtExeFact();
+    return fact;
+}
 
-	EXPORT_FLAG void deleteExecFact(IExecuterFact* fact)
-	{
-		if (fact != NULL)
-			delete fact;
-	}
+EXPORT_FLAG void deleteExecFact(IExecuterFact* fact)
+{
+    if (fact != NULL)
+        delete fact;
+}
 };
-
 
 WtExeFact::WtExeFact()
 {
 }
-
 
 WtExeFact::~WtExeFact()
 {
@@ -43,51 +40,51 @@ WtExeFact::~WtExeFact()
 
 const char* WtExeFact::getName()
 {
-	return FACT_NAME;
+    return FACT_NAME;
 }
 
 void WtExeFact::enumExeUnit(FuncEnumUnitCallback cb)
 {
-	cb(FACT_NAME, "WtTWapExeUnit", false);
-	cb(FACT_NAME, "WtMinImpactExeUnit", true);
+    cb(FACT_NAME, "WtTWapExeUnit", false);
+    cb(FACT_NAME, "WtMinImpactExeUnit", true);
 }
 
 ExecuteUnit* WtExeFact::createExeUnit(const char* name)
 {
-	if (strcmp(name, "WtTWapExeUnit") == 0)
-		return new WtTWapExeUnit();
-	else if (strcmp(name, "WtMinImpactExeUnit") == 0)
-		return new WtMinImpactExeUnit();
-	else if (strcmp(name, "WtStockMinImpactExeUnit") == 0)
-		return new WtStockMinImpactExeUnit();
-	else if (strcmp(name, "WtVWapExeUnit") == 0)
-		return  new WtVWapExeUnit();
-	else if (strcmp(name, "WtStockVWapExeUnit") == 0)
-		return new WtStockVWapExeUnit();
-	return NULL;
+    if (strcmp(name, "WtTWapExeUnit") == 0)
+        return new WtTWapExeUnit();
+    else if (strcmp(name, "WtMinImpactExeUnit") == 0)
+        return new WtMinImpactExeUnit();
+    else if (strcmp(name, "WtStockMinImpactExeUnit") == 0)
+        return new WtStockMinImpactExeUnit();
+    else if (strcmp(name, "WtVWapExeUnit") == 0)
+        return new WtVWapExeUnit();
+    else if (strcmp(name, "WtStockVWapExeUnit") == 0)
+        return new WtStockVWapExeUnit();
+    return NULL;
 }
 
 ExecuteUnit* WtExeFact::createDiffExeUnit(const char* name)
 {
-	if (strcmp(name, "WtDiffMinImpactExeUnit") == 0)
-		return new WtDiffMinImpactExeUnit();
+    if (strcmp(name, "WtDiffMinImpactExeUnit") == 0)
+        return new WtDiffMinImpactExeUnit();
 
-	return NULL;
+    return NULL;
 }
 
 ExecuteUnit* WtExeFact::createArbiExeUnit(const char* name)
 {
-	return NULL;
+    return NULL;
 }
 
 bool WtExeFact::deleteExeUnit(ExecuteUnit* unit)
 {
-	if (unit == NULL)
-		return true;
+    if (unit == NULL)
+        return true;
 
-	if (strcmp(unit->getFactName(), FACT_NAME) != 0)
-		return false;
+    if (strcmp(unit->getFactName(), FACT_NAME) != 0)
+        return false;
 
-	delete unit;
-	return true;
+    delete unit;
+    return true;
 }
