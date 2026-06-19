70889716279a34f63d4c5359d93c8a9a6a507735
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:02 +0800

    [style] reformat WtCtaStraFact/WtCtaStraFact.cpp (clang-format: brace style, tabs->spaces)

--- src/WtCtaStraFact/WtCtaStraFact.cpp
+++ src/WtCtaStraFact/WtCtaStraFact.cpp
@@ -6,27 +6,23 @@
 
 const char* FACT_NAME = "WtCtaStraFact";
 
-
-extern "C"
+extern "C" {
+EXPORT_FLAG ICtaStrategyFact* createStrategyFact()
 {
-	EXPORT_FLAG ICtaStrategyFact* createStrategyFact()
-	{
-		ICtaStrategyFact* fact = new WtStraFact();
-		return fact;
-	}
+    ICtaStrategyFact* fact = new WtStraFact();
+    return fact;
+}
 
-	EXPORT_FLAG void deleteStrategyFact(ICtaStrategyFact* fact)
-	{
-		if (fact != NULL)
-			delete fact;
-	}
+EXPORT_FLAG void deleteStrategyFact(ICtaStrategyFact* fact)
+{
+    if (fact != NULL)
+        delete fact;
+}
 };
-
 
 WtStraFact::WtStraFact()
 {
 }
-
 
 WtStraFact::~WtStraFact()
 {
@@ -34,30 +30,30 @@ WtStraFact::~WtStraFact()
 
 CtaStrategy* WtStraFact::createStrategy(const char* name, const char* id)
 {
-	if (strcmp(name, "DualThrust") == 0)
-		return new WtStraDualThrust(id);
+    if (strcmp(name, "DualThrust") == 0)
+        return new WtStraDualThrust(id);
 
-	return NULL;
+    return NULL;
 }
 
 bool WtStraFact::deleteStrategy(CtaStrategy* stra)
 {
-	if (stra == NULL)
-		return true;
+    if (stra == NULL)
+        return true;
 
-	if (strcmp(stra->getFactName(), FACT_NAME) != 0)
-		return false;
+    if (strcmp(stra->getFactName(), FACT_NAME) != 0)
+        return false;
 
-	delete stra;
-	return true;
+    delete stra;
+    return true;
 }
 
 void WtStraFact::enumStrategy(FuncEnumStrategyCallback cb)
 {
-	cb(FACT_NAME, "DualThrust", true);
+    cb(FACT_NAME, "DualThrust", true);
 }
 
 const char* WtStraFact::getName()
 {
-	return FACT_NAME;
+    return FACT_NAME;
 }
