8b9e92283eb7ee08c7f71ea919df6c996b063d44
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:53 +0800

    [style] reformat WTSUtils/yamlcpp/yaml-cpp/node/emit.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/yaml-cpp/node/emit.h
+++ src/WTSUtils/yamlcpp/yaml-cpp/node/emit.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -27,6 +27,6 @@ YAML_CPP_API std::ostream& operator<<(std::ostream& out, const Node& node);
 
 /** Converts the node to a YAML string. */
 YAML_CPP_API std::string Dump(const Node& node);
-}  // namespace YAML
+} // namespace YAML
 
-#endif  // NODE_EMIT_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // NODE_EMIT_H_62B23520_7C8E_11DE_8A39_0800200C9A66
