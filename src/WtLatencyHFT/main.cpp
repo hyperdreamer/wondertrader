2c5bbcaac0c57dcb9304af353b94774bb8fd4404
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtLatencyHFT/main.cpp (clang-format: brace style, tabs->spaces)

--- src/WtLatencyHFT/main.cpp
+++ src/WtLatencyHFT/main.cpp
@@ -4,8 +4,8 @@
  *
  * /author Wesley
  * /date 2020/03/30
- * 
- * /brief 
+ *
+ * /brief
  */
 
 #include "../WTSTools/WTSLogger.h"
@@ -14,9 +14,8 @@ extern void test_hft();
 
 int main()
 {
-	test_hft();
-	printf("press enter key to exit\r\n");
-	getchar();
-	return 0;
+    test_hft();
+    printf("press enter key to exit\r\n");
+    getchar();
+    return 0;
 }
-
