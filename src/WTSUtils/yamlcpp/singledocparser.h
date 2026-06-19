401f62622817599473582dc97433ffcbe5414488
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:51 +0800

    [style] reformat WTSUtils/yamlcpp/singledocparser.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/singledocparser.h
+++ src/WTSUtils/yamlcpp/singledocparser.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -15,7 +15,8 @@
 
 namespace YAML {
 class CollectionStack;
-template <int> class DepthGuard; // depthguard.h
+template<int>
+class DepthGuard; // depthguard.h
 class EventHandler;
 class Node;
 class Scanner;
@@ -24,48 +25,48 @@ struct Mark;
 struct Token;
 
 class SingleDocParser {
- public:
-  SingleDocParser(Scanner& scanner, const Directives& directives);
-  SingleDocParser(const SingleDocParser&) = delete;
-  SingleDocParser(SingleDocParser&&) = delete;
-  SingleDocParser& operator=(const SingleDocParser&) = delete;
-  SingleDocParser& operator=(SingleDocParser&&) = delete;
-  ~SingleDocParser();
+public:
+    SingleDocParser(Scanner& scanner, const Directives& directives);
+    SingleDocParser(const SingleDocParser&) = delete;
+    SingleDocParser(SingleDocParser&&) = delete;
+    SingleDocParser& operator=(const SingleDocParser&) = delete;
+    SingleDocParser& operator=(SingleDocParser&&) = delete;
+    ~SingleDocParser();
 
-  void HandleDocument(EventHandler& eventHandler);
+    void HandleDocument(EventHandler& eventHandler);
 
- private:
-  void HandleNode(EventHandler& eventHandler);
+private:
+    void HandleNode(EventHandler& eventHandler);
 
-  void HandleSequence(EventHandler& eventHandler);
-  void HandleBlockSequence(EventHandler& eventHandler);
-  void HandleFlowSequence(EventHandler& eventHandler);
+    void HandleSequence(EventHandler& eventHandler);
+    void HandleBlockSequence(EventHandler& eventHandler);
+    void HandleFlowSequence(EventHandler& eventHandler);
 
-  void HandleMap(EventHandler& eventHandler);
-  void HandleBlockMap(EventHandler& eventHandler);
-  void HandleFlowMap(EventHandler& eventHandler);
-  void HandleCompactMap(EventHandler& eventHandler);
-  void HandleCompactMapWithNoKey(EventHandler& eventHandler);
+    void HandleMap(EventHandler& eventHandler);
+    void HandleBlockMap(EventHandler& eventHandler);
+    void HandleFlowMap(EventHandler& eventHandler);
+    void HandleCompactMap(EventHandler& eventHandler);
+    void HandleCompactMapWithNoKey(EventHandler& eventHandler);
 
-  void ParseProperties(std::string& tag, anchor_t& anchor,
-                       std::string& anchor_name);
-  void ParseTag(std::string& tag);
-  void ParseAnchor(anchor_t& anchor, std::string& anchor_name);
+    void ParseProperties(std::string& tag, anchor_t& anchor,
+                         std::string& anchor_name);
+    void ParseTag(std::string& tag);
+    void ParseAnchor(anchor_t& anchor, std::string& anchor_name);
 
-  anchor_t RegisterAnchor(const std::string& name);
-  anchor_t LookupAnchor(const Mark& mark, const std::string& name) const;
+    anchor_t RegisterAnchor(const std::string& name);
+    anchor_t LookupAnchor(const Mark& mark, const std::string& name) const;
 
- private:
-  int depth = 0;
-  Scanner& m_scanner;
-  const Directives& m_directives;
-  std::unique_ptr<CollectionStack> m_pCollectionStack;
+private:
+    int depth = 0;
+    Scanner& m_scanner;
+    const Directives& m_directives;
+    std::unique_ptr<CollectionStack> m_pCollectionStack;
 
-  using Anchors = std::map<std::string, anchor_t>;
-  Anchors m_anchors;
+    using Anchors = std::map<std::string, anchor_t>;
+    Anchors m_anchors;
 
-  anchor_t m_curAnchor;
+    anchor_t m_curAnchor;
 };
-}  // namespace YAML
+} // namespace YAML
 
-#endif  // SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
