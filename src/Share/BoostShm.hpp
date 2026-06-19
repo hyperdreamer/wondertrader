49c6740039a1bef8e13edeb0f4a3f1e34bf97b72
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:47 +0800

    [style] reformat Share/BoostShm.hpp (clang-format: brace style, tabs->spaces)

--- src/Share/BoostShm.hpp
+++ src/Share/BoostShm.hpp
@@ -3,81 +3,76 @@
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 
-class BoostShm
-{
+class BoostShm {
 private:
-	std::string	_name;
-	boost::interprocess::shared_memory_object*	_obj;
-	boost::interprocess::mapped_region *		_region;
+    std::string _name;
+    boost::interprocess::shared_memory_object* _obj;
+    boost::interprocess::mapped_region* _region;
 
 public:
-	BoostShm(): _obj(nullptr), _region(nullptr){}
+    BoostShm(): _obj(nullptr), _region(nullptr) {}
 
-	~BoostShm()
-	{
-		close();
-	}
+    ~BoostShm()
+    {
+        close();
+    }
 
-	void close()
-	{
-		if (_region)
-			delete _region;
+    void close()
+    {
+        if (_region)
+            delete _region;
 
-		if (_obj)
-			delete _obj;
+        if (_obj)
+            delete _obj;
 
-		_obj = nullptr;
-		_region = nullptr;
-	}
+        _obj = nullptr;
+        _region = nullptr;
+    }
 
-	bool open(const char* name)
-	{
-		try
-		{
-			_obj = new boost::interprocess::shared_memory_object(boost::interprocess::open_only, name, boost::interprocess::read_write);
-			_region = new boost::interprocess::mapped_region(*_obj, boost::interprocess::read_write);
+    bool open(const char* name)
+    {
+        try {
+            _obj = new boost::interprocess::shared_memory_object(boost::interprocess::open_only, name, boost::interprocess::read_write);
+            _region = new boost::interprocess::mapped_region(*_obj, boost::interprocess::read_write);
 
-			return true;
-		}
-		catch(...)
-		{
-			return false;
-		}
-	}
+            return true;
+        }
+        catch (...) {
+            return false;
+        }
+    }
 
-	bool create(const char* name, std::size_t size)
-	{
-		try
-		{
-			boost::interprocess::shared_memory_object::remove(name);
-			_obj = new boost::interprocess::shared_memory_object(boost::interprocess::create_only, name, boost::interprocess::read_write);
-			_obj->truncate(size);
-			_region = new boost::interprocess::mapped_region(*_obj, boost::interprocess::read_write);
+    bool create(const char* name, std::size_t size)
+    {
+        try {
+            boost::interprocess::shared_memory_object::remove(name);
+            _obj = new boost::interprocess::shared_memory_object(boost::interprocess::create_only, name, boost::interprocess::read_write);
+            _obj->truncate(size);
+            _region = new boost::interprocess::mapped_region(*_obj, boost::interprocess::read_write);
 
-			return true;
-		}
-		catch (...)
-		{
-			return false;
-		}
-	}
+            return true;
+        }
+        catch (...) {
+            return false;
+        }
+    }
 
-	inline void *addr()
-	{
-		if (_region)
-			return _region->get_address();
-		return nullptr;
-	}
+    inline void* addr()
+    {
+        if (_region)
+            return _region->get_address();
+        return nullptr;
+    }
 
-	inline size_t size()
-	{
-		if (_region)
-			return _region->get_size();
-		return 0;
-	}
+    inline size_t size()
+    {
+        if (_region)
+            return _region->get_size();
+        return 0;
+    }
 
-	inline bool valid() const
-	{
-		return _obj != nullptr;
-	}
+    inline bool valid() const
+    {
+        return _obj != nullptr;
+    }
 };
