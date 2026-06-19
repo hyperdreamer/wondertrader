a91855b05a311f8e73dcbc14ffdf00245396658b
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:54 +0800

    [style] reformat WTSUtils/yamlcpp/yaml-cpp/node/parse.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/yaml-cpp/node/parse.h
+++ src/WTSUtils/yamlcpp/yaml-cpp/node/parse.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -73,6 +73,6 @@ YAML_CPP_API std::vector<Node> LoadAll(std::istream& input);
  * @throws {@link BadFile} if the file cannot be loaded.
  */
 YAML_CPP_API std::vector<Node> LoadAllFromFile(const std::string& filename);
-}  // namespace YAML
+} // namespace YAML
 
-#endif  // VALUE_PARSE_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // VALUE_PARSE_H_62B23520_7C8E_11DE_8A39_0800200C9A66
