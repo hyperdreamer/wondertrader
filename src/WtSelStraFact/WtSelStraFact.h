27271d66f8d84c16a83ccfd3433d7a3729947725
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtSelStraFact/WtSelStraFact.h (clang-format: brace style, tabs->spaces)

--- src/WtSelStraFact/WtSelStraFact.h
+++ src/WtSelStraFact/WtSelStraFact.h
@@ -3,19 +3,17 @@
 
 USING_NS_WTP;
 
-class WtSelStraFact : public ISelStrategyFact
-{
+class WtSelStraFact : public ISelStrategyFact {
 public:
-	WtSelStraFact();
-	virtual ~WtSelStraFact();
+    WtSelStraFact();
+    virtual ~WtSelStraFact();
 
 public:
-	virtual const char* getName() override;
+    virtual const char* getName() override;
 
-	virtual SelStrategy* createStrategy(const char* name, const char* id) override;
+    virtual SelStrategy* createStrategy(const char* name, const char* id) override;
 
-	virtual void enumStrategy(FuncEnumSelStrategyCallback cb) override;
+    virtual void enumStrategy(FuncEnumSelStrategyCallback cb) override;
 
-	virtual bool deleteStrategy(SelStrategy* stra) override;
+    virtual bool deleteStrategy(SelStrategy* stra) override;
 };
-
