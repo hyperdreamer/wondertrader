d3438043f36295d82200a20f46ad7c6c9a0c7a9c
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtIndexFactory/WtIndexFactory.h (clang-format: brace style, tabs->spaces)

--- src/WtIndexFactory/WtIndexFactory.h
+++ src/WtIndexFactory/WtIndexFactory.h
@@ -3,17 +3,15 @@
 
 USING_NS_WTP;
 
-class WtIdxFactory : public IIndexWorkerFact
-{
+class WtIdxFactory : public IIndexWorkerFact {
 public:
-	WtIdxFactory();
-	virtual ~WtIdxFactory();
+    WtIdxFactory();
+    virtual ~WtIdxFactory();
 
 public:
-	virtual const char* get_name() override;
+    virtual const char* get_name() override;
 
-	virtual IIndexWorker* create_worker(const char* name, const char* id) override;
+    virtual IIndexWorker* create_worker(const char* name, const char* id) override;
 
-	virtual bool delete_worker(IIndexWorker* worker) override;
+    virtual bool delete_worker(IIndexWorker* worker) override;
 };
-
