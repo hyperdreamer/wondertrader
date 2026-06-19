226e4ec6a410190559a2f37f38354fb8d839d183
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:49 +0800

    [style] reformat TraderOES/TraderOES.h (clang-format: brace style, tabs->spaces)

--- src/TraderOES/TraderOES.h
+++ src/TraderOES/TraderOES.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 
@@ -23,105 +23,102 @@
 
 USING_NS_WTP;
 
-class TraderOES : public ITraderApi
-{
+class TraderOES : public ITraderApi {
 public:
-	TraderOES();
-	virtual ~TraderOES();
+    TraderOES();
+    virtual ~TraderOES();
 
-	typedef enum
-	{
-		TS_NOTLOGIN,		//未登录
-		TS_LOGINING,		//正在登录
-		TS_LOGINED,			//已登录
-		TS_LOGINFAILED,		//登录失败
-		TS_ALLREADY			//全部就绪
-	} TraderState;
+    typedef enum {
+        TS_NOTLOGIN,    // 未登录
+        TS_LOGINING,    // 正在登录
+        TS_LOGINED,     // 已登录
+        TS_LOGINFAILED, // 登录失败
+        TS_ALLREADY     // 全部就绪
+    } TraderState;
 
 public:
-	void	handle_ord_connected(OesAsyncApiChannelT *pAsyncChannel);
-	void	handle_ord_disconnected(OesAsyncApiChannelT *pAsyncChannel);
-	void	handle_rpt_connected(OesAsyncApiChannelT *pAsyncChannel);
-	void	handle_rpt_disconnected(OesAsyncApiChannelT *pAsyncChannel);
-	void	handle_rpt_message(SMsgHeadT *pMsgHead, void *pMsgItem);
-	void	handle_ord_message(SMsgHeadT *pMsgHead, void *pMsgItem);
+    void handle_ord_connected(OesAsyncApiChannelT* pAsyncChannel);
+    void handle_ord_disconnected(OesAsyncApiChannelT* pAsyncChannel);
+    void handle_rpt_connected(OesAsyncApiChannelT* pAsyncChannel);
+    void handle_rpt_disconnected(OesAsyncApiChannelT* pAsyncChannel);
+    void handle_rpt_message(SMsgHeadT* pMsgHead, void* pMsgItem);
+    void handle_ord_message(SMsgHeadT* pMsgHead, void* pMsgItem);
 
-	void	handle_rsp_account(SMsgHeadT *pMsgHead, void *pMsgItem, OesQryCursorT *pQryCursor);
-	void	handle_rsp_orders(SMsgHeadT *pMsgHead, void *pMsgItem, OesQryCursorT *pQryCursor);
-	void	handle_rsp_trades(SMsgHeadT *pMsgHead, void *pMsgItem, OesQryCursorT *pQryCursor);
-	void	handle_rsp_positions(SMsgHeadT *pMsgHead, void *pMsgItem, OesQryCursorT *pQryCursor);
+    void handle_rsp_account(SMsgHeadT* pMsgHead, void* pMsgItem, OesQryCursorT* pQryCursor);
+    void handle_rsp_orders(SMsgHeadT* pMsgHead, void* pMsgItem, OesQryCursorT* pQryCursor);
+    void handle_rsp_trades(SMsgHeadT* pMsgHead, void* pMsgItem, OesQryCursorT* pQryCursor);
+    void handle_rsp_positions(SMsgHeadT* pMsgHead, void* pMsgItem, OesQryCursorT* pQryCursor);
 
 public:
-	//////////////////////////////////////////////////////////////////////////
-	//ITraderApi 接口
-	virtual bool init(WTSVariant *params) override;
+    //////////////////////////////////////////////////////////////////////////
+    // ITraderApi 接口
+    virtual bool init(WTSVariant* params) override;
 
-	virtual void release() override;
+    virtual void release() override;
 
-	virtual void registerSpi(ITraderSpi *listener) override;
+    virtual void registerSpi(ITraderSpi* listener) override;
 
-	virtual void connect() override;
+    virtual void connect() override;
 
-	virtual void disconnect() override;
+    virtual void disconnect() override;
 
-	virtual bool isConnected() override;
+    virtual bool isConnected() override;
 
-	virtual bool makeEntrustID(char* buffer, int length) override;
+    virtual bool makeEntrustID(char* buffer, int length) override;
 
-	virtual int login(const char* user, const char* pass, const char* productInfo) override;
+    virtual int login(const char* user, const char* pass, const char* productInfo) override;
 
-	virtual int logout() override;
+    virtual int logout() override;
 
-	virtual int orderInsert(WTSEntrust* eutrust) override;
+    virtual int orderInsert(WTSEntrust* eutrust) override;
 
-	virtual int orderAction(WTSEntrustAction* action) override;
+    virtual int orderAction(WTSEntrustAction* action) override;
 
-	virtual int queryAccount() override;
+    virtual int queryAccount() override;
 
-	virtual int queryPositions() override;
+    virtual int queryPositions() override;
 
-	virtual int queryOrders() override;
+    virtual int queryOrders() override;
 
-	virtual int queryTrades() override;
+    virtual int queryTrades() override;
 
 private:
-	void		reconnect();
-	void		doLogin();
+    void reconnect();
+    void doLogin();
 
-	inline WTSOrderInfo*	makeOrderInfo(OesOrdCnfmT* orderField);
-	inline WTSEntrust*		makeEntrust(OesOrdRejectT *entrustField);
-	inline WTSTradeInfo*	makeTradeInfo(OesTrdCnfmT *tradeField);
+    inline WTSOrderInfo* makeOrderInfo(OesOrdCnfmT* orderField);
+    inline WTSEntrust* makeEntrust(OesOrdRejectT* entrustField);
+    inline WTSTradeInfo* makeTradeInfo(OesTrdCnfmT* tradeField);
 
-	inline bool	extractEntrustID(const char* entrustid, uint32_t &orderRef);
-	inline void	genEntrustID(char* buffer, uint32_t orderRef);
+    inline bool extractEntrustID(const char* entrustid, uint32_t& orderRef);
+    inline void genEntrustID(char* buffer, uint32_t orderRef);
 
 private:
-	OesAsyncApiContextT*	_context;
-	OesAsyncApiChannelT*	_channel;
-	OesAsyncApiChannelT*	_channel_qry;
-	ITraderSpi*				_sink;
+    OesAsyncApiContextT* _context;
+    OesAsyncApiChannelT* _channel;
+    OesAsyncApiChannelT* _channel_qry;
+    ITraderSpi* _sink;
 
-	IBaseDataMgr*			_bd_mgr;
+    IBaseDataMgr* _bd_mgr;
 
-	WTSArray*		_trades;
-	WTSArray*		_orders;
-	WTSArray*		_funds;
-	WTSArray*		_positions;
+    WTSArray* _trades;
+    WTSArray* _orders;
+    WTSArray* _funds;
+    WTSArray* _positions;
 
-	std::string		_config;
-	std::string		_user;
-	bool			_inited;
+    std::string _config;
+    std::string _user;
+    bool _inited;
 
-	TraderState		_state;
+    TraderState _state;
 
-	uint32_t		_tradingday;
+    uint32_t _tradingday;
 
-	boost::asio::io_service		_asyncio;
-	StdThreadPtr				_thrd_worker;
+    boost::asio::io_service _asyncio;
+    StdThreadPtr _thrd_worker;
 
-	//委托单标记缓存器
-	WtKVCache		m_eidCache;
-	//订单标记缓存器
-	WtKVCache		m_oidCache;
+    // 委托单标记缓存器
+    WtKVCache m_eidCache;
+    // 订单标记缓存器
+    WtKVCache m_oidCache;
 };
-
