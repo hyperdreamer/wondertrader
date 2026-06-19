5a30e8e3eaedefc68b143f6158b5a8d078c049ff
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:50 +0800

    [style] reformat WTSUtils/yamlcpp/emitfromevents.cpp (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/emitfromevents.cpp
+++ src/WTSUtils/yamlcpp/emitfromevents.cpp
@@ -8,15 +8,16 @@
 
 namespace YAML {
 struct Mark;
-}  // namespace YAML
+} // namespace YAML
 
 namespace {
-std::string ToString(YAML::anchor_t anchor) {
-  std::stringstream stream;
-  stream << anchor;
-  return stream.str();
+std::string ToString(YAML::anchor_t anchor)
+{
+    std::stringstream stream;
+    stream << anchor;
+    return stream.str();
 }
-}  // namespace
+} // namespace
 
 namespace YAML {
 EmitFromEvents::EmitFromEvents(Emitter& emitter)
@@ -26,99 +27,108 @@ void EmitFromEvents::OnDocumentStart(const Mark&) {}
 
 void EmitFromEvents::OnDocumentEnd() {}
 
-void EmitFromEvents::OnNull(const Mark&, anchor_t anchor) {
-  BeginNode();
-  EmitProps("", anchor);
-  m_emitter << Null;
+void EmitFromEvents::OnNull(const Mark&, anchor_t anchor)
+{
+    BeginNode();
+    EmitProps("", anchor);
+    m_emitter << Null;
 }
 
-void EmitFromEvents::OnAlias(const Mark&, anchor_t anchor) {
-  BeginNode();
-  m_emitter << Alias(ToString(anchor));
+void EmitFromEvents::OnAlias(const Mark&, anchor_t anchor)
+{
+    BeginNode();
+    m_emitter << Alias(ToString(anchor));
 }
 
 void EmitFromEvents::OnScalar(const Mark&, const std::string& tag,
-                              anchor_t anchor, const std::string& value) {
-  BeginNode();
-  EmitProps(tag, anchor);
-  m_emitter << value;
+                              anchor_t anchor, const std::string& value)
+{
+    BeginNode();
+    EmitProps(tag, anchor);
+    m_emitter << value;
 }
 
 void EmitFromEvents::OnSequenceStart(const Mark&, const std::string& tag,
                                      anchor_t anchor,
-                                     EmitterStyle::value style) {
-  BeginNode();
-  EmitProps(tag, anchor);
-  switch (style) {
+                                     EmitterStyle::value style)
+{
+    BeginNode();
+    EmitProps(tag, anchor);
+    switch (style) {
     case EmitterStyle::Block:
-      m_emitter << Block;
-      break;
+        m_emitter << Block;
+        break;
     case EmitterStyle::Flow:
-      m_emitter << Flow;
-      break;
+        m_emitter << Flow;
+        break;
     default:
-      break;
-  }
-  // Restore the global settings to eliminate the override from node style
-  m_emitter.RestoreGlobalModifiedSettings();
-  m_emitter << BeginSeq;
-  m_stateStack.push(State::WaitingForSequenceEntry);
+        break;
+    }
+    // Restore the global settings to eliminate the override from node style
+    m_emitter.RestoreGlobalModifiedSettings();
+    m_emitter << BeginSeq;
+    m_stateStack.push(State::WaitingForSequenceEntry);
 }
 
-void EmitFromEvents::OnSequenceEnd() {
-  m_emitter << EndSeq;
-  assert(m_stateStack.top() == State::WaitingForSequenceEntry);
-  m_stateStack.pop();
+void EmitFromEvents::OnSequenceEnd()
+{
+    m_emitter << EndSeq;
+    assert(m_stateStack.top() == State::WaitingForSequenceEntry);
+    m_stateStack.pop();
 }
 
 void EmitFromEvents::OnMapStart(const Mark&, const std::string& tag,
-                                anchor_t anchor, EmitterStyle::value style) {
-  BeginNode();
-  EmitProps(tag, anchor);
-  switch (style) {
+                                anchor_t anchor, EmitterStyle::value style)
+{
+    BeginNode();
+    EmitProps(tag, anchor);
+    switch (style) {
     case EmitterStyle::Block:
-      m_emitter << Block;
-      break;
+        m_emitter << Block;
+        break;
     case EmitterStyle::Flow:
-      m_emitter << Flow;
-      break;
+        m_emitter << Flow;
+        break;
     default:
-      break;
-  }
-  // Restore the global settings to eliminate the override from node style
-  m_emitter.RestoreGlobalModifiedSettings();
-  m_emitter << BeginMap;
-  m_stateStack.push(State::WaitingForKey);
+        break;
+    }
+    // Restore the global settings to eliminate the override from node style
+    m_emitter.RestoreGlobalModifiedSettings();
+    m_emitter << BeginMap;
+    m_stateStack.push(State::WaitingForKey);
 }
 
-void EmitFromEvents::OnMapEnd() {
-  m_emitter << EndMap;
-  assert(m_stateStack.top() == State::WaitingForKey);
-  m_stateStack.pop();
+void EmitFromEvents::OnMapEnd()
+{
+    m_emitter << EndMap;
+    assert(m_stateStack.top() == State::WaitingForKey);
+    m_stateStack.pop();
 }
 
-void EmitFromEvents::BeginNode() {
-  if (m_stateStack.empty())
-    return;
+void EmitFromEvents::BeginNode()
+{
+    if (m_stateStack.empty())
+        return;
 
-  switch (m_stateStack.top()) {
+    switch (m_stateStack.top()) {
     case State::WaitingForKey:
-      m_emitter << Key;
-      m_stateStack.top() = State::WaitingForValue;
-      break;
+        m_emitter << Key;
+        m_stateStack.top() = State::WaitingForValue;
+        break;
     case State::WaitingForValue:
-      m_emitter << Value;
-      m_stateStack.top() = State::WaitingForKey;
-      break;
+        m_emitter << Value;
+        m_stateStack.top() = State::WaitingForKey;
+        break;
     default:
-      break;
-  }
+        break;
+    }
 }
 
-void EmitFromEvents::EmitProps(const std::string& tag, anchor_t anchor) {
-  if (!tag.empty() && tag != "?" && tag != "!")
-    m_emitter << VerbatimTag(tag);
-  if (anchor)
-    m_emitter << Anchor(ToString(anchor));
+void EmitFromEvents::EmitProps(const std::string& tag, anchor_t anchor)
+{
+    if (!tag.empty() && tag != "?" && tag != "!")
+        m_emitter << VerbatimTag(tag);
+    if (anchor)
+        m_emitter << Anchor(ToString(anchor));
 }
-}  // namespace YAML
+} // namespace YAML
