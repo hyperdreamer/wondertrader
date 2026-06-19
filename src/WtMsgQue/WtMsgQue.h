88a7c603c06e98826548a7e77072adb0c9e61fa9
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtMsgQue/WtMsgQue.h (clang-format: brace style, tabs->spaces)

--- src/WtMsgQue/WtMsgQue.h
+++ src/WtMsgQue/WtMsgQue.h
@@ -4,26 +4,25 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include "PorterDefs.h"
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-	EXPORT_FLAG void		regiter_callbacks(FuncLogCallback cbLog);
+EXPORT_FLAG void regiter_callbacks(FuncLogCallback cbLog);
 
-	EXPORT_FLAG WtUInt32	create_server(const char* url, bool confirm);
-	EXPORT_FLAG void		destroy_server(WtUInt32 id);
-	EXPORT_FLAG void		publish_message(WtUInt32 id, const char* topic, const char* data, WtUInt32 dataLen);
+EXPORT_FLAG WtUInt32 create_server(const char* url, bool confirm);
+EXPORT_FLAG void destroy_server(WtUInt32 id);
+EXPORT_FLAG void publish_message(WtUInt32 id, const char* topic, const char* data, WtUInt32 dataLen);
 
-	EXPORT_FLAG WtUInt32	create_client(const char* url, FuncMQCallback cb);
-	EXPORT_FLAG void		destroy_client(WtUInt32 id);
-	EXPORT_FLAG void		subscribe_topic(WtUInt32 id, const char* topic);
-	EXPORT_FLAG void		start_client(WtUInt32 id);
+EXPORT_FLAG WtUInt32 create_client(const char* url, FuncMQCallback cb);
+EXPORT_FLAG void destroy_client(WtUInt32 id);
+EXPORT_FLAG void subscribe_topic(WtUInt32 id, const char* topic);
+EXPORT_FLAG void start_client(WtUInt32 id);
 #ifdef __cplusplus
 }
 #endif
