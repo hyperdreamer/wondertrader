b04f8aad0160e1a63a6f37fd694b4b4c6280bf58
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:49 +0800

    [style] reformat TraderDumper/TraderDumper.h (clang-format: brace style, tabs->spaces)

--- src/TraderDumper/TraderDumper.h
+++ src/TraderDumper/TraderDumper.h
@@ -3,18 +3,17 @@
 #include "PorterDefs.h"
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-	EXPORT_FLAG	void		register_callbacks(FuncOnAccount cbAccount, FuncOnOrder  cbOrder, FuncOnTrade cbTrade, FuncOnPosition cbPosition);
+EXPORT_FLAG void register_callbacks(FuncOnAccount cbAccount, FuncOnOrder cbOrder, FuncOnTrade cbTrade, FuncOnPosition cbPosition);
 
-	EXPORT_FLAG	void		init(const char* logProfile);
+EXPORT_FLAG void init(const char* logProfile);
 
-	EXPORT_FLAG	bool		config(const char* cfgfile, bool isFile);
+EXPORT_FLAG bool config(const char* cfgfile, bool isFile);
 
-	EXPORT_FLAG	void		run(bool bOnce);
+EXPORT_FLAG void run(bool bOnce);
 
-	EXPORT_FLAG	void		release();
+EXPORT_FLAG void release();
 
 #ifdef __cplusplus
 }
