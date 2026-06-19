cb08781bbb4341808929756fe753ae5f7fd2d525
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:55 +0800

    [style] reformat WTSUtils/yamlcpp/yaml.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/yaml.h
+++ src/WTSUtils/yamlcpp/yaml.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -21,4 +21,4 @@
 #include "yaml-cpp/node/parse.h"
 #include "yaml-cpp/node/emit.h"
 
-#endif  // YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66
