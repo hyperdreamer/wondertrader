dc56dd785200b4dd5785df8c67babcfc3762a0cc
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftCore/WtUftTicker.h (clang-format: brace style, tabs->spaces)

--- src/WtUftCore/WtUftTicker.h
+++ src/WtUftCore/WtUftTicker.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 
@@ -21,34 +21,33 @@ class WTSTickData;
 
 class WtUftEngine;
 
-class WtUftRtTicker
-{
+class WtUftRtTicker {
 public:
-	WtUftRtTicker(WtUftEngine* engine);
-	~WtUftRtTicker();
+    WtUftRtTicker(WtUftEngine* engine);
+    ~WtUftRtTicker();
 
 public:
-	void	init(const char* sessionID);
-	void	on_tick(WTSTickData* curTick);
+    void init(const char* sessionID);
+    void on_tick(WTSTickData* curTick);
 
-	void	run();
-	void	stop();
+    void run();
+    void stop();
 
 private:
-	WTSSessionInfo*	_s_info;
-	WtUftEngine*	_engine;
+    WTSSessionInfo* _s_info;
+    WtUftEngine* _engine;
 
-	uint32_t	_date;
-	uint32_t	_time;
+    uint32_t _date;
+    uint32_t _time;
 
-	uint32_t	_cur_pos;
+    uint32_t _cur_pos;
 
-	StdUniqueMutex	_mtx;
-	std::atomic<uint64_t>	_next_check_time;
-	std::atomic<uint32_t>	_last_emit_pos;
+    StdUniqueMutex _mtx;
+    std::atomic<uint64_t> _next_check_time;
+    std::atomic<uint32_t> _last_emit_pos;
 
-	bool			_stopped;
-	StdThreadPtr	_thrd;
+    bool _stopped;
+    StdThreadPtr _thrd;
 };
 
 NS_WTP_END
