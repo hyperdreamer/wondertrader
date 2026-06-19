fc52da4c4b2727e6debc40bd6332a1b29b47727f
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:01 +0800

    [style] reformat WtCore/WtExecuterFactory.h (clang-format: brace style, tabs->spaces)

--- src/WtCore/WtExecuterFactory.h
+++ src/WtCore/WtExecuterFactory.h
@@ -8,63 +8,57 @@
 NS_WTP_BEGIN
 
 //////////////////////////////////////////////////////////////////////////
-//执行单元封装
-//因为执行单元是dll里创建的, 如果不封装的话, 直接delete可能会有问题
-//所以要把工厂指针一起封装到这里, 直接调用工厂实例的deleteUnit方法释放执行单元
-class ExeUnitWrapper
-{
+// 执行单元封装
+// 因为执行单元是dll里创建的, 如果不封装的话, 直接delete可能会有问题
+// 所以要把工厂指针一起封装到这里, 直接调用工厂实例的deleteUnit方法释放执行单元
+class ExeUnitWrapper {
 public:
-	ExeUnitWrapper(ExecuteUnit* unitPtr, IExecuterFact* fact) :_unit(unitPtr), _fact(fact) {}
-	~ExeUnitWrapper()
-	{
-		if (_unit)
-		{
-			_fact->deleteExeUnit(_unit);
-		}
-	}
+    ExeUnitWrapper(ExecuteUnit* unitPtr, IExecuterFact* fact): _unit(unitPtr), _fact(fact) {}
+    ~ExeUnitWrapper()
+    {
+        if (_unit) {
+            _fact->deleteExeUnit(_unit);
+        }
+    }
 
-	ExecuteUnit* self() { return _unit; }
-
+    ExecuteUnit* self() { return _unit; }
 
 private:
-	ExecuteUnit*	_unit;
-	IExecuterFact*	_fact;
+    ExecuteUnit* _unit;
+    IExecuterFact* _fact;
 };
 
-typedef std::shared_ptr<ExeUnitWrapper>	ExecuteUnitPtr;
+typedef std::shared_ptr<ExeUnitWrapper> ExecuteUnitPtr;
 typedef wt_hashmap<std::string, ExecuteUnitPtr> ExecuteUnitMap;
 
 //////////////////////////////////////////////////////////////////////////
-//执行器工厂类
-class WtExecuterFactory : private boost::noncopyable
-{
+// 执行器工厂类
+class WtExecuterFactory : private boost::noncopyable {
 public:
-	~WtExecuterFactory() {}
+    ~WtExecuterFactory() {}
 
 public:
-	bool loadFactories(const char* path);
+    bool loadFactories(const char* path);
 
-	ExecuteUnitPtr createExeUnit(const char* name);
-	ExecuteUnitPtr createDiffExeUnit(const char* name);
-	ExecuteUnitPtr createArbiExeUnit(const char* name);
+    ExecuteUnitPtr createExeUnit(const char* name);
+    ExecuteUnitPtr createDiffExeUnit(const char* name);
+    ExecuteUnitPtr createArbiExeUnit(const char* name);
 
-	ExecuteUnitPtr createExeUnit(const char* factname, const char* unitname);
-	ExecuteUnitPtr createDiffExeUnit(const char* factname, const char* unitname);
-	ExecuteUnitPtr createArbiExeUnit(const char* factname, const char* unitname);
+    ExecuteUnitPtr createExeUnit(const char* factname, const char* unitname);
+    ExecuteUnitPtr createDiffExeUnit(const char* factname, const char* unitname);
+    ExecuteUnitPtr createArbiExeUnit(const char* factname, const char* unitname);
 
 private:
-	typedef struct _ExeFactInfo
-	{
-		std::string		_module_path;
-		DllHandle		_module_inst;
-		IExecuterFact*	_fact;
-		FuncCreateExeFact	_creator;
-		FuncDeleteExeFact	_remover;
-	} ExeFactInfo;
-	typedef wt_hashmap<std::string, ExeFactInfo> ExeFactMap;
+    typedef struct _ExeFactInfo {
+        std::string _module_path;
+        DllHandle _module_inst;
+        IExecuterFact* _fact;
+        FuncCreateExeFact _creator;
+        FuncDeleteExeFact _remover;
+    } ExeFactInfo;
+    typedef wt_hashmap<std::string, ExeFactInfo> ExeFactMap;
 
-	ExeFactMap	_factories;
+    ExeFactMap _factories;
 };
-
 
 NS_WTP_END
