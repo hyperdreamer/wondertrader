358725487639ba145f7c9794b5f7fa432c4e9edc
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:50 +0800

    [style] reformat WTSUtils/yamlcpp/directives.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/directives.h
+++ src/WTSUtils/yamlcpp/directives.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -12,18 +12,18 @@
 
 namespace YAML {
 struct Version {
-  bool isDefault;
-  int major, minor;
+    bool isDefault;
+    int major, minor;
 };
 
 struct Directives {
-  Directives();
+    Directives();
 
-  const std::string TranslateTagHandle(const std::string& handle) const;
+    const std::string TranslateTagHandle(const std::string& handle) const;
 
-  Version version;
-  std::map<std::string, std::string> tags;
+    Version version;
+    std::map<std::string, std::string> tags;
 };
-}
+} // namespace YAML
 
-#endif  // DIRECTIVES_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // DIRECTIVES_H_62B23520_7C8E_11DE_8A39_0800200C9A66
