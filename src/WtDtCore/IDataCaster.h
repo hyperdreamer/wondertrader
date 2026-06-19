4fca5e287add7d269853e0448ce0a9ad554de6e7
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:02 +0800

    [style] reformat WtDtCore/IDataCaster.h (clang-format: brace style, tabs->spaces)

--- src/WtDtCore/IDataCaster.h
+++ src/WtDtCore/IDataCaster.h
@@ -8,13 +8,12 @@ class WTSOrdDtlData;
 class WTSOrdQueData;
 class WTSTransData;
 
-class IDataCaster
-{
+class IDataCaster {
 public:
-	virtual void	broadcast(WTSTickData* curTick) = 0;
-	virtual void	broadcast(WTSOrdQueData* curOrdQue){}
-	virtual void	broadcast(WTSOrdDtlData* curOrdDtl){}
-	virtual void	broadcast(WTSTransData* curTrans){}
+    virtual void broadcast(WTSTickData* curTick) = 0;
+    virtual void broadcast(WTSOrdQueData* curOrdQue) {}
+    virtual void broadcast(WTSOrdDtlData* curOrdDtl) {}
+    virtual void broadcast(WTSTransData* curTrans) {}
 };
 
 NS_WTP_END
