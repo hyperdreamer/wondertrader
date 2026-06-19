61e821da674ddea66f1e9936e17e31c30e7cafcc
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftCore/WtHelper.cpp (clang-format: brace style, tabs->spaces)

--- src/WtUftCore/WtHelper.cpp
+++ src/WtUftCore/WtHelper.cpp
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #include "WtHelper.h"
 
@@ -17,7 +17,7 @@ namespace fs = std::filesystem;
 
 #ifdef _MSC_VER
 #include <direct.h>
-#else	//UNIX
+#else // UNIX
 #include <unistd.h>
 #endif
 
@@ -28,66 +28,64 @@ uint32_t WtHelper::_cur_tdate = 0;
 std::string WtHelper::_inst_dir;
 std::string WtHelper::_gen_dir = "./generated/";
 
-
 std::string WtHelper::getCWD()
 {
-	static std::string _cwd;
-	if(_cwd.empty())
-	{
-		char   buffer[256];
+    static std::string _cwd;
+    if (_cwd.empty()) {
+        char buffer[256];
 #ifdef _MSC_VER
-		_getcwd(buffer, 255);
-#else	//UNIX
-		getcwd(buffer, 255);
+        _getcwd(buffer, 255);
+#else // UNIX
+        getcwd(buffer, 255);
 #endif
-		_cwd = StrUtil::standardisePath(buffer);
-	}	
-	return _cwd;
+        _cwd = StrUtil::standardisePath(buffer);
+    }
+    return _cwd;
 }
 
 std::string WtHelper::getModulePath(const char* moduleName, const char* subDir, bool isCWD /* = true */)
 {
-	std::stringstream ss;
-	ss << (isCWD?getCWD():getInstDir()) << subDir << "/" << moduleName;
-	return ss.str();
+    std::stringstream ss;
+    ss << (isCWD ? getCWD() : getInstDir()) << subDir << "/" << moduleName;
+    return ss.str();
 }
 
 const char* WtHelper::getStraDataDir()
 {
-	static std::string folder = StrUtil::standardisePath(_gen_dir) + "stradata/";
-	if (!StdFile::exists(folder.c_str()))
-		fs::create_directories(folder);
-	return folder.c_str();
+    static std::string folder = StrUtil::standardisePath(_gen_dir) + "stradata/";
+    if (!StdFile::exists(folder.c_str()))
+        fs::create_directories(folder);
+    return folder.c_str();
 }
 
 const char* WtHelper::getStraUsrDatDir()
 {
-	static std::string folder = StrUtil::standardisePath(_gen_dir) + "userdata/";
-	if (!StdFile::exists(folder.c_str()))
-		fs::create_directories(folder);
-	return folder.c_str();
+    static std::string folder = StrUtil::standardisePath(_gen_dir) + "userdata/";
+    if (!StdFile::exists(folder.c_str()))
+        fs::create_directories(folder);
+    return folder.c_str();
 }
 
 const char* WtHelper::getPortifolioDir()
 {
-	static std::string folder = StrUtil::standardisePath(_gen_dir) + "portfolio/";
-	if (!StdFile::exists(folder.c_str()))
-		fs::create_directories(folder);
-	return folder.c_str();
+    static std::string folder = StrUtil::standardisePath(_gen_dir) + "portfolio/";
+    if (!StdFile::exists(folder.c_str()))
+        fs::create_directories(folder);
+    return folder.c_str();
 }
 
 const char* WtHelper::getOutputDir()
 {
-	static std::string folder = StrUtil::standardisePath(_gen_dir) + "outputs/";
-	if (!StdFile::exists(folder.c_str()))
-		fs::create_directories(folder);
-	return folder.c_str();
+    static std::string folder = StrUtil::standardisePath(_gen_dir) + "outputs/";
+    if (!StdFile::exists(folder.c_str()))
+        fs::create_directories(folder);
+    return folder.c_str();
 }
 
 const char* WtHelper::getBaseDir()
 {
-	static std::string folder = StrUtil::standardisePath(_gen_dir);
-	if (!StdFile::exists(folder.c_str()))
-		fs::create_directories(folder);
-	return folder.c_str();
+    static std::string folder = StrUtil::standardisePath(_gen_dir);
+    if (!StdFile::exists(folder.c_str()))
+        fs::create_directories(folder);
+    return folder.c_str();
 }
