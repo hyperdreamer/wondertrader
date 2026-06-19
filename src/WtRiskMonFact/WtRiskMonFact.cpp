1ab436299f08477cadd8c481c4015886229ab377
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtRiskMonFact/WtRiskMonFact.cpp (clang-format: brace style, tabs->spaces)

--- src/WtRiskMonFact/WtRiskMonFact.cpp
+++ src/WtRiskMonFact/WtRiskMonFact.cpp
@@ -4,33 +4,30 @@
  * \author Wesley
  * \date 2020/03/30
  *
- * 
+ *
  */
 #include "WtRiskMonFact.h"
 #include "WtSimpRiskMon.h"
 
 const char* FACT_NAME = "WtRiskMonFact";
 
-extern "C"
+extern "C" {
+EXPORT_FLAG IRiskMonitorFact* createRiskMonFact()
 {
-	EXPORT_FLAG IRiskMonitorFact* createRiskMonFact()
-	{
-		IRiskMonitorFact* fact = new WtRiskMonFact();
-		return fact;
-	}
-
-	EXPORT_FLAG void deleteRiskMonFact(IRiskMonitorFact* fact)
-	{
-		if (fact != NULL)
-			delete fact;
-	}
+    IRiskMonitorFact* fact = new WtRiskMonFact();
+    return fact;
 }
 
+EXPORT_FLAG void deleteRiskMonFact(IRiskMonitorFact* fact)
+{
+    if (fact != NULL)
+        delete fact;
+}
+}
 
 WtRiskMonFact::WtRiskMonFact()
 {
 }
-
 
 WtRiskMonFact::~WtRiskMonFact()
 {
@@ -38,30 +35,30 @@ WtRiskMonFact::~WtRiskMonFact()
 
 const char* WtRiskMonFact::getName()
 {
-	return FACT_NAME;
+    return FACT_NAME;
 }
 
 void WtRiskMonFact::enumRiskMonitors(FuncEnumRiskMonCallback cb)
 {
-	//cb(FACT_NAME, "WtSimpExeUnit", false);
-	cb(FACT_NAME, "SimpleRiskMon", true);
+    // cb(FACT_NAME, "WtSimpExeUnit", false);
+    cb(FACT_NAME, "SimpleRiskMon", true);
 }
 
 WtRiskMonitor* WtRiskMonFact::createRiskMonotor(const char* name)
 {
-	if (strcmp(name, "SimpleRiskMon") == 0)
-		return new WtSimpleRiskMon();
-	return NULL;
+    if (strcmp(name, "SimpleRiskMon") == 0)
+        return new WtSimpleRiskMon();
+    return NULL;
 }
 
 bool WtRiskMonFact::deleteRiskMonotor(WtRiskMonitor* unit)
 {
-	if (unit == NULL)
-		return true;
+    if (unit == NULL)
+        return true;
 
-	if (strcmp(unit->getFactName(), FACT_NAME) != 0)
-		return false;
+    if (strcmp(unit->getFactName(), FACT_NAME) != 0)
+        return false;
 
-	delete unit;
-	return true;
+    delete unit;
+    return true;
 }
