a22371d9af2667e7345ef06c953bea3e8a85472e
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:51 +0800

    [style] reformat WTSUtils/yamlcpp/tag.cpp (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/tag.cpp
+++ src/WTSUtils/yamlcpp/tag.cpp
@@ -1,50 +1,52 @@
 #include <cassert>
 #include <stdexcept>
 
-#include "directives.h"  // IWYU pragma: keep
+#include "directives.h" // IWYU pragma: keep
 #include "tag.h"
 #include "token.h"
 
 namespace YAML {
 Tag::Tag(const Token& token)
-    : type(static_cast<TYPE>(token.data)), handle{}, value{} {
-  switch (type) {
+    : type(static_cast<TYPE>(token.data)), handle{}, value{}
+{
+    switch (type) {
     case VERBATIM:
-      value = token.value;
-      break;
+        value = token.value;
+        break;
     case PRIMARY_HANDLE:
-      value = token.value;
-      break;
+        value = token.value;
+        break;
     case SECONDARY_HANDLE:
-      value = token.value;
-      break;
+        value = token.value;
+        break;
     case NAMED_HANDLE:
-      handle = token.value;
-      value = token.params[0];
-      break;
+        handle = token.value;
+        value = token.params[0];
+        break;
     case NON_SPECIFIC:
-      break;
+        break;
     default:
-      assert(false);
-  }
+        assert(false);
+    }
 }
 
-const std::string Tag::Translate(const Directives& directives) {
-  switch (type) {
+const std::string Tag::Translate(const Directives& directives)
+{
+    switch (type) {
     case VERBATIM:
-      return value;
+        return value;
     case PRIMARY_HANDLE:
-      return directives.TranslateTagHandle("!") + value;
+        return directives.TranslateTagHandle("!") + value;
     case SECONDARY_HANDLE:
-      return directives.TranslateTagHandle("!!") + value;
+        return directives.TranslateTagHandle("!!") + value;
     case NAMED_HANDLE:
-      return directives.TranslateTagHandle("!" + handle + "!") + value;
+        return directives.TranslateTagHandle("!" + handle + "!") + value;
     case NON_SPECIFIC:
-      // TODO:
-      return "!";
+        // TODO:
+        return "!";
     default:
-      assert(false);
-  }
-  throw std::runtime_error("yaml-cpp: internal error, bad tag type");
+        assert(false);
+    }
+    throw std::runtime_error("yaml-cpp: internal error, bad tag type");
 }
-}  // namespace YAML
+} // namespace YAML
