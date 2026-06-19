92d41ad78949f445e7907bf1b64dd5bac0820413
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:01 +0800

    [style] reformat WtCore/IExecCommand.h (clang-format: brace style, tabs->spaces)

--- src/WtCore/IExecCommand.h
+++ src/WtCore/IExecCommand.h
@@ -8,44 +8,41 @@ class WTSSessionInfo;
 class IHotMgr;
 class WTSTickData;
 
-class IExecuterStub
-{
+class IExecuterStub {
 public:
-	virtual uint64_t get_real_time() = 0;
-	virtual WTSCommodityInfo* get_comm_info(const char* stdCode) = 0;
-	virtual WTSSessionInfo* get_sess_info(const char* stdCode) = 0;
-	virtual IHotMgr* get_hot_mon() = 0;
-	virtual uint32_t get_trading_day() = 0;
+    virtual uint64_t get_real_time() = 0;
+    virtual WTSCommodityInfo* get_comm_info(const char* stdCode) = 0;
+    virtual WTSSessionInfo* get_sess_info(const char* stdCode) = 0;
+    virtual IHotMgr* get_hot_mon() = 0;
+    virtual uint32_t get_trading_day() = 0;
 };
 
-class IExecCommand
-{
+class IExecCommand {
 public:
-	IExecCommand(const char* name) :_stub(NULL), _name(name){}
-	/*
-	 *	设置目标仓位
-	 */
-	virtual void set_position(const wt_hashmap<std::string, double>& targets) {}
+    IExecCommand(const char* name): _stub(NULL), _name(name) {}
+    /*
+     *	设置目标仓位
+     */
+    virtual void set_position(const wt_hashmap<std::string, double>& targets) {}
 
-	/*
-	 *	合约仓位变动
-	 */
-	virtual void on_position_changed(const char* stdCode, double diffPos) {}
+    /*
+     *	合约仓位变动
+     */
+    virtual void on_position_changed(const char* stdCode, double diffPos) {}
 
-	/*
-	 *	实时行情回调
-	 */
-	virtual void on_tick(const char* stdCode, WTSTickData* newTick) {}
+    /*
+     *	实时行情回调
+     */
+    virtual void on_tick(const char* stdCode, WTSTickData* newTick) {}
 
+    inline void setStub(IExecuterStub* stub) { _stub = stub; }
 
-	inline void setStub(IExecuterStub* stub) { _stub = stub; }
+    inline const char* name() const { return _name.c_str(); }
 
-	inline const char* name() const { return _name.c_str(); }
-
-	inline void setName(const char* name) { _name = name; }
+    inline void setName(const char* name) { _name = name; }
 
 protected:
-	IExecuterStub*	_stub;
-	std::string		_name;
+    IExecuterStub* _stub;
+    std::string _name;
 };
 NS_WTP_END
