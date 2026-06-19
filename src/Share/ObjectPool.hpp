e8e2f23a54e1e77e09dda745570fc87b284fc88c
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:47 +0800

    [style] reformat Share/ObjectPool.hpp (clang-format: brace style, tabs->spaces)

--- src/Share/ObjectPool.hpp
+++ src/Share/ObjectPool.hpp
@@ -2,35 +2,33 @@
 #include <boost/pool/pool.hpp>
 #include <atomic>
 
-template < typename T>
-class ObjectPool
-{
-	boost::pool<> _pool;
+template<typename T>
+class ObjectPool {
+    boost::pool<> _pool;
 
 public:
-	ObjectPool() :_pool(sizeof(T)) {}
-	virtual ~ObjectPool() {}
+    ObjectPool(): _pool(sizeof(T)) {}
+    virtual ~ObjectPool() {}
 
-	T* construct()
-	{
-		void * mem = _pool.malloc();
-		if (!mem)
-			return nullptr;
+    T* construct()
+    {
+        void* mem = _pool.malloc();
+        if (!mem)
+            return nullptr;
 
-		T* pobj = new(mem) T();
-		return pobj;
-	}
+        T* pobj = new (mem) T();
+        return pobj;
+    }
 
-	void destroy(T* pobj)
-	{
-		pobj->~T();
-		_pool.free(pobj);
-	}
+    void destroy(T* pobj)
+    {
+        pobj->~T();
+        _pool.free(pobj);
+    }
 
-	//手动释放未使用的内存
-	void release()
-	{
-		_pool.release_memory();
-	}
+    // 手动释放未使用的内存
+    void release()
+    {
+        _pool.release_memory();
+    }
 };
-
