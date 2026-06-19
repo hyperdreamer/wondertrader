fba88387fe9c2086eea32bde25bcdd6155ea0b8f
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtIndexFactory/WtBlkIdxWorker.h (clang-format: brace style, tabs->spaces)

--- src/WtIndexFactory/WtBlkIdxWorker.h
+++ src/WtIndexFactory/WtBlkIdxWorker.h
@@ -15,47 +15,45 @@ class IHotMgr;
 class IBaseDataMgr;
 class WTSContractInfo;
 
-class WtBlkIdxWorker : public IIndexWorker
-{
+class WtBlkIdxWorker : public IIndexWorker {
 public:
-	WtBlkIdxWorker(const char* id):IIndexWorker(id), _stopped(false), _process(false) {}
+    WtBlkIdxWorker(const char* id): IIndexWorker(id), _stopped(false), _process(false) {}
 
 public:
-	virtual bool		init(WTSVariant* config) override;
-	virtual void		handle_quote(WTSTickData* newTick) override;
-	virtual const char* get_fact_name() override;
+    virtual bool init(WTSVariant* config) override;
+    virtual void handle_quote(WTSTickData* newTick) override;
+    virtual const char* get_fact_name() override;
 
 private:
-	void	generate_tick();
+    void generate_tick();
 
 protected:
-	std::string		_exchg;
-	std::string		_code;
-	std::string		_trigger;
-	uint32_t		_timeout;
-	uint64_t		_recalc_time;
-	double			_stand_scale;
-	WTSTickStruct	_cache;
-	WTSContractInfo*	_cInfo;
+    std::string _exchg;
+    std::string _code;
+    std::string _trigger;
+    uint32_t _timeout;
+    uint64_t _recalc_time;
+    double _stand_scale;
+    WTSTickStruct _cache;
+    WTSContractInfo* _cInfo;
 
-	typedef struct _WeightFactor
-	{
-		double			_weight;
-		WTSTickStruct	_tick;
-		_WeightFactor()
-		{
-			memset(this, 0, sizeof(_WeightFactor));
-		}
-	}WeightFactor;
-	SpinMutex	_mtx_data;
-	wt_hashmap<std::string, WeightFactor>	_weight_scales;
-	uint32_t	_weight_alg;
+    typedef struct _WeightFactor {
+        double _weight;
+        WTSTickStruct _tick;
+        _WeightFactor()
+        {
+            memset(this, 0, sizeof(_WeightFactor));
+        }
+    } WeightFactor;
+    SpinMutex _mtx_data;
+    wt_hashmap<std::string, WeightFactor> _weight_scales;
+    uint32_t _weight_alg;
 
-	StdThreadPtr	_thrd_trigger;
-	StdUniqueMutex	_mtx_trigger;
-	StdCondVariable	_cond_trigger;
-	bool			_stopped;
-	bool			_process;
+    StdThreadPtr _thrd_trigger;
+    StdUniqueMutex _mtx_trigger;
+    StdCondVariable _cond_trigger;
+    bool _stopped;
+    bool _process;
 };
 
 NS_WTP_END
