e92aa22d7cef9573ed08a6c1355b157d6738e8a9
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:52 +0800

    [style] reformat WTSUtils/yamlcpp/yaml-cpp/node/detail/iterator_fwd.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/yaml-cpp/node/detail/iterator_fwd.h
+++ src/WTSUtils/yamlcpp/yaml-cpp/node/detail/iterator_fwd.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -16,12 +16,12 @@ namespace YAML {
 
 namespace detail {
 struct iterator_value;
-template <typename V>
+template<typename V>
 class iterator_base;
-}
+} // namespace detail
 
 using iterator = detail::iterator_base<detail::iterator_value>;
 using const_iterator = detail::iterator_base<const detail::iterator_value>;
-}
+} // namespace YAML
 
-#endif  // VALUE_DETAIL_ITERATOR_FWD_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+#endif // VALUE_DETAIL_ITERATOR_FWD_H_62B23520_7C8E_11DE_8A39_0800200C9A66
