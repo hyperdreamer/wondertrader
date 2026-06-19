9557849ae57667526c049942dc8b3fea600aad8c
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftCore/UftStrategyMgr.h (clang-format: brace style, tabs->spaces)

--- src/WtUftCore/UftStrategyMgr.h
+++ src/WtUftCore/UftStrategyMgr.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <memory>
@@ -16,68 +16,62 @@
 
 #include "../Share/DLLHelper.hpp"
 
-class UftStraWrapper
-{
+class UftStraWrapper {
 public:
-	UftStraWrapper(UftStrategy* stra, IUftStrategyFact* fact) :_stra(stra), _fact(fact){}
-	~UftStraWrapper()
-	{
-		if (_stra)
-		{
-			_fact->deleteStrategy(_stra);
-		}
-	}
+    UftStraWrapper(UftStrategy* stra, IUftStrategyFact* fact): _stra(stra), _fact(fact) {}
+    ~UftStraWrapper()
+    {
+        if (_stra) {
+            _fact->deleteStrategy(_stra);
+        }
+    }
 
-	UftStrategy* self(){ return _stra; }
-
+    UftStrategy* self() { return _stra; }
 
 private:
-	UftStrategy*		_stra;
-	IUftStrategyFact*	_fact;
+    UftStrategy* _stra;
+    IUftStrategyFact* _fact;
 };
 
-typedef std::shared_ptr<UftStraWrapper>	UftStrategyPtr;
+typedef std::shared_ptr<UftStraWrapper> UftStrategyPtr;
 
-class UftStrategyMgr : private boost::noncopyable
-{
+class UftStrategyMgr : private boost::noncopyable {
 public:
-	UftStrategyMgr();
-	~UftStrategyMgr();
+    UftStrategyMgr();
+    ~UftStrategyMgr();
 
 public:
-	bool loadFactories(const char* path);
+    bool loadFactories(const char* path);
 
-	UftStrategyPtr createStrategy(const char* name, const char* id);
-	UftStrategyPtr createStrategy(const char* factname, const char* unitname, const char* id);
+    UftStrategyPtr createStrategy(const char* name, const char* id);
+    UftStrategyPtr createStrategy(const char* factname, const char* unitname, const char* id);
 
-	UftStrategyPtr getStrategy(const char* id);
+    UftStrategyPtr getStrategy(const char* id);
 
 private:
-	typedef struct _StraFactInfo
-	{
-		std::string		_module_path;
-		DllHandle		_module_inst;
-		IUftStrategyFact*	_fact;
-		FuncCreateUftStraFact	_creator;
-		FuncDeleteUftStraFact	_remover;
+    typedef struct _StraFactInfo {
+        std::string _module_path;
+        DllHandle _module_inst;
+        IUftStrategyFact* _fact;
+        FuncCreateUftStraFact _creator;
+        FuncDeleteUftStraFact _remover;
 
-		_StraFactInfo()
-		{
-			_module_inst = NULL;
-			_fact = NULL;
-		}
+        _StraFactInfo()
+        {
+            _module_inst = NULL;
+            _fact = NULL;
+        }
 
-		~_StraFactInfo()
-		{
-			if (_fact)
-				_remover(_fact);
-		}
-	} StraFactInfo;
-	typedef wt_hashmap<std::string, StraFactInfo> StraFactMap;
+        ~_StraFactInfo()
+        {
+            if (_fact)
+                _remover(_fact);
+        }
+    } StraFactInfo;
+    typedef wt_hashmap<std::string, StraFactInfo> StraFactMap;
 
-	StraFactMap	_factories;
+    StraFactMap _factories;
 
-	typedef wt_hashmap<std::string, UftStrategyPtr> StrategyMap;
-	StrategyMap	_strategies;
+    typedef wt_hashmap<std::string, UftStrategyPtr> StrategyMap;
+    StrategyMap _strategies;
 };
-
