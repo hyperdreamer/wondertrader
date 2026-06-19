1771c815277ad61a21fcbfdd7a402e0f058a21ea
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:03 +0800

    [style] reformat WtExecMon/WtExecPorter.h (clang-format: brace style, tabs->spaces)

--- src/WtExecMon/WtExecPorter.h
+++ src/WtExecMon/WtExecPorter.h
@@ -2,28 +2,27 @@
 #include <stdint.h>
 #include "../Includes/WTSMarcos.h"
 
-typedef const char*			WtString;
+typedef const char* WtString;
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-	EXPORT_FLAG	void		init_exec(WtString logCfg, bool isFile = true);
+EXPORT_FLAG void init_exec(WtString logCfg, bool isFile = true);
 
-	EXPORT_FLAG	void		config_exec(WtString cfgfile, bool isFile = true);
+EXPORT_FLAG void config_exec(WtString cfgfile, bool isFile = true);
 
-	EXPORT_FLAG	void		run_exec();
+EXPORT_FLAG void run_exec();
 
-	EXPORT_FLAG	void		write_log(unsigned int level, WtString message, WtString catName);
+EXPORT_FLAG void write_log(unsigned int level, WtString message, WtString catName);
 
-	EXPORT_FLAG	WtString	get_version();
+EXPORT_FLAG WtString get_version();
 
-	EXPORT_FLAG	void		release_exec();
+EXPORT_FLAG void release_exec();
 
-	EXPORT_FLAG	void		set_position(WtString stdCode, double targetPos);
+EXPORT_FLAG void set_position(WtString stdCode, double targetPos);
 
-	EXPORT_FLAG	void		commit_positions();
+EXPORT_FLAG void commit_positions();
 
 #ifdef __cplusplus
 }
