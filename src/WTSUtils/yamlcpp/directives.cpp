370f665e7436fdf1238d542919d40a6428bb9d8f
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:50 +0800

    [style] reformat WTSUtils/yamlcpp/directives.cpp (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/directives.cpp
+++ src/WTSUtils/yamlcpp/directives.cpp
@@ -1,17 +1,18 @@
 #include "directives.h"
 
 namespace YAML {
-Directives::Directives() : version{true, 1, 2}, tags{} {}
+Directives::Directives(): version{ true, 1, 2 }, tags{} {}
 
 const std::string Directives::TranslateTagHandle(
-    const std::string& handle) const {
-  auto it = tags.find(handle);
-  if (it == tags.end()) {
-    if (handle == "!!")
-      return "tag:yaml.org,2002:";
-    return handle;
-  }
+    const std::string& handle) const
+{
+    auto it = tags.find(handle);
+    if (it == tags.end()) {
+        if (handle == "!!")
+            return "tag:yaml.org,2002:";
+        return handle;
+    }
 
-  return it->second;
+    return it->second;
 }
-}  // namespace YAML
+} // namespace YAML
