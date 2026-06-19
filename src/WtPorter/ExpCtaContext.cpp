c9b03c5fbf13e56f614bce14666cbbfde273de7c
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtPorter/ExpCtaContext.cpp (clang-format: brace style, tabs->spaces)

--- src/WtPorter/ExpCtaContext.cpp
+++ src/WtPorter/ExpCtaContext.cpp
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #include "ExpCtaContext.h"
 #include "WtRtRunner.h"
@@ -15,10 +15,9 @@
 extern WtRtRunner& getRunner();
 
 ExpCtaContext::ExpCtaContext(WtCtaEngine* env, const char* name, int32_t slippage)
-	: CtaStraBaseCtx(env, name, slippage)
+    : CtaStraBaseCtx(env, name, slippage)
 {
 }
-
 
 ExpCtaContext::~ExpCtaContext()
 {
@@ -26,51 +25,51 @@ ExpCtaContext::~ExpCtaContext()
 
 void ExpCtaContext::on_init()
 {
-	CtaStraBaseCtx::on_init();
+    CtaStraBaseCtx::on_init();
 
-	//向外部回调
-	getRunner().ctx_on_init(_context_id, ET_CTA);
+    // 向外部回调
+    getRunner().ctx_on_init(_context_id, ET_CTA);
 
-	dump_chart_info();
+    dump_chart_info();
 }
 
 void ExpCtaContext::on_session_begin(uint32_t uDate)
 {
-	CtaStraBaseCtx::on_session_begin(uDate);
+    CtaStraBaseCtx::on_session_begin(uDate);
 
-	//向外部回调
-	getRunner().ctx_on_session_event(_context_id, uDate, true, ET_CTA);
+    // 向外部回调
+    getRunner().ctx_on_session_event(_context_id, uDate, true, ET_CTA);
 }
 
 void ExpCtaContext::on_session_end(uint32_t uDate)
 {
-	//向外部回调
-	getRunner().ctx_on_session_event(_context_id, uDate, false, ET_CTA);
+    // 向外部回调
+    getRunner().ctx_on_session_event(_context_id, uDate, false, ET_CTA);
 
-	CtaStraBaseCtx::on_session_end(uDate);
+    CtaStraBaseCtx::on_session_end(uDate);
 }
 
 void ExpCtaContext::on_tick_updated(const char* stdCode, WTSTickData* newTick)
 {
-	auto it = _tick_subs.find(stdCode);
-	if (it == _tick_subs.end())
-		return;
+    auto it = _tick_subs.find(stdCode);
+    if (it == _tick_subs.end())
+        return;
 
-	getRunner().ctx_on_tick(_context_id, stdCode, newTick, ET_CTA);
+    getRunner().ctx_on_tick(_context_id, stdCode, newTick, ET_CTA);
 }
 
 void ExpCtaContext::on_bar_close(const char* stdCode, const char* period, WTSBarStruct* newBar)
 {
-	//要向外部回调
-	getRunner().ctx_on_bar(_context_id, stdCode, period, newBar, ET_CTA);
+    // 要向外部回调
+    getRunner().ctx_on_bar(_context_id, stdCode, period, newBar, ET_CTA);
 }
 
 void ExpCtaContext::on_calculate(uint32_t curDate, uint32_t curTime)
 {
-	getRunner().ctx_on_calc(_context_id, curDate, curTime, ET_CTA);
+    getRunner().ctx_on_calc(_context_id, curDate, curTime, ET_CTA);
 }
 
 void ExpCtaContext::on_condition_triggered(const char* stdCode, double target, double price, const char* usertag)
 {
-	getRunner().ctx_on_cond_triggered(_context_id, stdCode, target, price, usertag, ET_CTA);
+    getRunner().ctx_on_cond_triggered(_context_id, stdCode, target, price, usertag, ET_CTA);
 }
