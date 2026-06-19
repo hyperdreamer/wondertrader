f482ae8a0df44b852ec37e9ea23d503bec208c84
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:50 +0800

    [style] reformat WTSUtils/yamlcpp/node.cpp (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/node.cpp
+++ src/WTSUtils/yamlcpp/node.cpp
@@ -3,10 +3,11 @@
 #include "nodeevents.h"
 
 namespace YAML {
-Node Clone(const Node& node) {
-  NodeEvents events(node);
-  NodeBuilder builder;
-  events.Emit(builder);
-  return builder.Root();
+Node Clone(const Node& node)
+{
+    NodeEvents events(node);
+    NodeBuilder builder;
+    events.Emit(builder);
+    return builder.Root();
 }
-}  // namespace YAML
+} // namespace YAML
