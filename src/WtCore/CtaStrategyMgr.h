3e0e40f6cc0bf9eb39e9437f659ed62008c85bc1
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:01 +0800

    [style] reformat WtCore/CtaStrategyMgr.h (clang-format: brace style, tabs->spaces)

--- src/WtCore/CtaStrategyMgr.h
+++ src/WtCore/CtaStrategyMgr.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <memory>
@@ -16,55 +16,49 @@
 
 #include "../Share/DLLHelper.hpp"
 
-class CtaStraWrapper
-{
+class CtaStraWrapper {
 public:
-	CtaStraWrapper(CtaStrategy* stra, ICtaStrategyFact* fact) :_stra(stra), _fact(fact){}
-	~CtaStraWrapper()
-	{
-		if (_stra)
-		{
-			_fact->deleteStrategy(_stra);
-		}
-	}
+    CtaStraWrapper(CtaStrategy* stra, ICtaStrategyFact* fact): _stra(stra), _fact(fact) {}
+    ~CtaStraWrapper()
+    {
+        if (_stra) {
+            _fact->deleteStrategy(_stra);
+        }
+    }
 
-	CtaStrategy* self(){ return _stra; }
-
+    CtaStrategy* self() { return _stra; }
 
 private:
-	CtaStrategy*		_stra;
-	ICtaStrategyFact*	_fact;
+    CtaStrategy* _stra;
+    ICtaStrategyFact* _fact;
 };
-typedef std::shared_ptr<CtaStraWrapper>	CtaStrategyPtr;
+typedef std::shared_ptr<CtaStraWrapper> CtaStrategyPtr;
 
-
-class CtaStrategyMgr : private boost::noncopyable
-{
+class CtaStrategyMgr : private boost::noncopyable {
 public:
-	CtaStrategyMgr();
-	~CtaStrategyMgr();
+    CtaStrategyMgr();
+    ~CtaStrategyMgr();
 
 public:
-	bool loadFactories(const char* path);
+    bool loadFactories(const char* path);
 
-	CtaStrategyPtr createStrategy(const char* name, const char* id);
-	CtaStrategyPtr createStrategy(const char* factname, const char* unitname, const char* id);
+    CtaStrategyPtr createStrategy(const char* name, const char* id);
+    CtaStrategyPtr createStrategy(const char* factname, const char* unitname, const char* id);
 
-	CtaStrategyPtr getStrategy(const char* id);
+    CtaStrategyPtr getStrategy(const char* id);
+
 private:
-	typedef struct _StraFactInfo
-	{
-		std::string		_module_path;
-		DllHandle		_module_inst;
-		ICtaStrategyFact*	_fact;
-		FuncCreateStraFact	_creator;
-		FuncDeleteStraFact	_remover;
-	} StraFactInfo;
-	typedef wt_hashmap<std::string, StraFactInfo> StraFactMap;
+    typedef struct _StraFactInfo {
+        std::string _module_path;
+        DllHandle _module_inst;
+        ICtaStrategyFact* _fact;
+        FuncCreateStraFact _creator;
+        FuncDeleteStraFact _remover;
+    } StraFactInfo;
+    typedef wt_hashmap<std::string, StraFactInfo> StraFactMap;
 
-	StraFactMap	_factories;
+    StraFactMap _factories;
 
-	typedef wt_hashmap<std::string, CtaStrategyPtr> StrategyMap;
-	StrategyMap	_strategies;
+    typedef wt_hashmap<std::string, CtaStrategyPtr> StrategyMap;
+    StrategyMap _strategies;
 };
-
