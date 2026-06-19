fd36b694a10eba5ae22118a378bef8ebe4ed71e5
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftCore/WtUftDtMgr.h (clang-format: brace style, tabs->spaces)

--- src/WtUftCore/WtUftDtMgr.h
+++ src/WtUftCore/WtUftDtMgr.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <vector>
@@ -24,44 +24,42 @@ class IBaseDataMgr;
 class IBaseDataMgr;
 class WtUftEngine;
 
-class WtUftDtMgr : public IDataManager
-{
+class WtUftDtMgr : public IDataManager {
 public:
-	WtUftDtMgr();
-	~WtUftDtMgr();
+    WtUftDtMgr();
+    ~WtUftDtMgr();
 
 public:
-	bool	init(WTSVariant* cfg, WtUftEngine* engine);
+    bool init(WTSVariant* cfg, WtUftEngine* engine);
 
-	void	handle_push_quote(const char* stdCode, WTSTickData* newTick);
+    void handle_push_quote(const char* stdCode, WTSTickData* newTick);
 
-	//////////////////////////////////////////////////////////////////////////
-	//IDataManager 接口
-	virtual WTSTickSlice* get_tick_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
-	virtual WTSOrdQueSlice* get_order_queue_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
-	virtual WTSOrdDtlSlice* get_order_detail_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
-	virtual WTSTransSlice* get_transaction_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
-	virtual WTSKlineSlice* get_kline_slice(const char* stdCode, WTSKlinePeriod period, uint32_t times, uint32_t count, uint64_t etime = 0) override;
-	virtual WTSTickData* grab_last_tick(const char* stdCode) override;
+    //////////////////////////////////////////////////////////////////////////
+    // IDataManager 接口
+    virtual WTSTickSlice* get_tick_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
+    virtual WTSOrdQueSlice* get_order_queue_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
+    virtual WTSOrdDtlSlice* get_order_detail_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
+    virtual WTSTransSlice* get_transaction_slice(const char* stdCode, uint32_t count, uint64_t etime = 0) override;
+    virtual WTSKlineSlice* get_kline_slice(const char* stdCode, WTSKlinePeriod period, uint32_t times, uint32_t count, uint64_t etime = 0) override;
+    virtual WTSTickData* grab_last_tick(const char* stdCode) override;
 
 private:
-	WtUftEngine*		_engine;
+    WtUftEngine* _engine;
 
-	wt_hashset<std::string> _subed_basic_bars;
-	typedef WTSHashMap<std::string> DataCacheMap;
-	DataCacheMap*	_bars_cache;	//K线缓存
-	DataCacheMap*	_ticks_cache;	//历史Tick缓存
-	DataCacheMap*	_rt_tick_map;	//实时tick缓存
+    wt_hashset<std::string> _subed_basic_bars;
+    typedef WTSHashMap<std::string> DataCacheMap;
+    DataCacheMap* _bars_cache;  // K线缓存
+    DataCacheMap* _ticks_cache; // 历史Tick缓存
+    DataCacheMap* _rt_tick_map; // 实时tick缓存
 
-	typedef struct _NotifyItem
-	{
-		std::string _code;
-		std::string _period;
-		uint32_t	_times;
-		WTSBarStruct* _newBar;
-	} NotifyItem;
+    typedef struct _NotifyItem {
+        std::string _code;
+        std::string _period;
+        uint32_t _times;
+        WTSBarStruct* _newBar;
+    } NotifyItem;
 
-	std::vector<NotifyItem> _bar_notifies;
+    std::vector<NotifyItem> _bar_notifies;
 };
 
 NS_WTP_END
