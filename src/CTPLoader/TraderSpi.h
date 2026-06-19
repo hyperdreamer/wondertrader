eda0b9bb5740cde9d6f622df640f0b25782fe4e9
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:45 +0800

    [style] reformat CTPLoader/TraderSpi.h (clang-format: brace style, tabs->spaces)

--- src/CTPLoader/TraderSpi.h
+++ src/CTPLoader/TraderSpi.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-//v6.3.15
+// v6.3.15
 #include "../API/CTP6.3.15/ThostFtdcTraderApi.h"
 #include "../Share/StdUtils.hpp"
 #include "../Share/SpinMutex.hpp"
@@ -13,66 +13,65 @@ using namespace loader;
 
 typedef std::function<bool()> QueryTask;
 
-class CTraderSpi : public CThostFtdcTraderSpi
-{
+class CTraderSpi : public CThostFtdcTraderSpi {
 public:
-	CTraderSpi():_stopped(false){}
-	~CTraderSpi()
-	{
-		_stopped = true;
-		if (_worker)
-			_worker->join();
-	}
+    CTraderSpi(): _stopped(false) {}
+    ~CTraderSpi()
+    {
+        _stopped = true;
+        if (_worker)
+            _worker->join();
+    }
 
 public:
-	///当客户端与交易后台建立起通信连接时（还未登录前）,该方法被调用。
-	virtual void OnFrontConnected();
+    /// 当客户端与交易后台建立起通信连接时（还未登录前）,该方法被调用。
+    virtual void OnFrontConnected();
 
-	virtual void OnRspAuthenticate(CThostFtdcRspAuthenticateField *pRspAuthenticateField, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspAuthenticate(CThostFtdcRspAuthenticateField* pRspAuthenticateField, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
 
-	///登录请求响应
-	virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,	CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-	
-	///请求查询合约响应
-	virtual void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    /// 登录请求响应
+    virtual void OnRspUserLogin(CThostFtdcRspUserLoginField* pRspUserLogin, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
 
-	virtual void OnRspQryInstrumentCommissionRate(CThostFtdcInstrumentCommissionRateField *pInstrumentCommissionRate, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+    /// 请求查询合约响应
+    virtual void OnRspQryInstrument(CThostFtdcInstrumentField* pInstrument, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
 
-	virtual void OnRspQryInstrumentMarginRate(CThostFtdcInstrumentMarginRateField *pInstrumentMarginRate, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) override;
+    virtual void OnRspQryInstrumentCommissionRate(CThostFtdcInstrumentCommissionRateField* pInstrumentCommissionRate, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast) override;
 
-	///错误应答
-	virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-	
-	///当客户端与交易后台通信连接断开时,该方法被调用。当发生这个情况后,API会自动重新连接,客户端可不做处理。
-	virtual void OnFrontDisconnected(int nReason);
+    virtual void OnRspQryInstrumentMarginRate(CThostFtdcInstrumentMarginRateField* pInstrumentMarginRate, CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast) override;
+
+    /// 错误应答
+    virtual void OnRspError(CThostFtdcRspInfoField* pRspInfo, int nRequestID, bool bIsLast);
+
+    /// 当客户端与交易后台通信连接断开时,该方法被调用。当发生这个情况后,API会自动重新连接,客户端可不做处理。
+    virtual void OnFrontDisconnected(int nReason);
 
 private:
-	void ReqAuth();
-	///用户登录请求
-	void ReqUserLogin();
-	///请求查询合约
-	void ReqQryInstrument();
+    void ReqAuth();
+    /// 用户登录请求
+    void ReqUserLogin();
+    /// 请求查询合约
+    void ReqQryInstrument();
 
-	void ReqQryCommission(const Contract& cInfo);
+    void ReqQryCommission(const Contract& cInfo);
 
-	void ReqQryMargin(const Contract& cInfo);
+    void ReqQryMargin(const Contract& cInfo);
 
-	// 是否收到成功的响应
-	bool IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo);
+    // 是否收到成功的响应
+    bool IsErrorRspInfo(CThostFtdcRspInfoField* pRspInfo);
 
-	void DumpToJson();
+    void DumpToJson();
 
-	void DumpFees();
+    void DumpFees();
 
-	void LoadFromJson();
+    void LoadFromJson();
 
-	void AppendQuery(const QueryTask& task);
+    void AppendQuery(const QueryTask& task);
 
 private:
-	int		_trading_day;
+    int _trading_day;
 
-	std::queue<QueryTask>	_queries;
-	SpinMutex		_mtx;
-	std::shared_ptr<boost::thread>	_worker;
-	bool			_stopped;
+    std::queue<QueryTask> _queries;
+    SpinMutex _mtx;
+    std::shared_ptr<boost::thread> _worker;
+    bool _stopped;
 };
