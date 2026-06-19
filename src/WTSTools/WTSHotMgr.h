2bdf154708b9eff4a8c2f94d3bd036ec8487bf00
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:49 +0800

    [style] reformat WTSTools/WTSHotMgr.h (clang-format: brace style, tabs->spaces)

--- src/WTSTools/WTSHotMgr.h
+++ src/WTSTools/WTSHotMgr.h
@@ -4,7 +4,7 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
+ *
  * \brief 主力合约管理器实现
  */
 #pragma once
@@ -14,83 +14,80 @@
 #include <string>
 
 NS_WTP_BEGIN
-	class WTSSwitchItem;
+class WTSSwitchItem;
 NS_WTP_END
 
 USING_NS_WTP;
 
-//换月主力映射
-typedef WTSMap<uint32_t>			WTSDateHotMap;
-//品种主力映射
-typedef WTSHashMap<std::string>		WTSProductHotMap;
-//分市场主力映射
-typedef WTSHashMap<std::string>		WTSExchgHotMap;
+// 换月主力映射
+typedef WTSMap<uint32_t> WTSDateHotMap;
+// 品种主力映射
+typedef WTSHashMap<std::string> WTSProductHotMap;
+// 分市场主力映射
+typedef WTSHashMap<std::string> WTSExchgHotMap;
 
-//自定义切换规则映射
-typedef WTSHashMap<std::string>		WTSCustomSwitchMap;
+// 自定义切换规则映射
+typedef WTSHashMap<std::string> WTSCustomSwitchMap;
 
-class WTSHotMgr : public IHotMgr
-{
+class WTSHotMgr : public IHotMgr {
 public:
-	WTSHotMgr();
-	~WTSHotMgr();
-
-public:
-	bool loadHots(const char* filename);
-	bool loadSeconds(const char* filename);
-	void release();
-
-	bool loadCustomRules(const char* tag, const char* filename);
-
-	inline bool isInitialized() const {return m_bInitialized;}
-
-	const std::vector<std::string>& getHotTags() const;
+    WTSHotMgr();
+    ~WTSHotMgr();
 
 public:
-	virtual const char* getRuleTag(const char* stdCode) override;
+    bool loadHots(const char* filename);
+    bool loadSeconds(const char* filename);
+    void release();
 
-	virtual double		getRuleFactor(const char* ruleTag, const char* fullPid, uint32_t uDate  = 0 ) override;
+    bool loadCustomRules(const char* tag, const char* filename);
 
-	//////////////////////////////////////////////////////////////////////////
-	//主力接口
-	virtual const char* getRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
+    inline bool isInitialized() const { return m_bInitialized; }
 
-	virtual const char* getPrevRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
+    const std::vector<std::string>& getHotTags() const;
 
-	virtual bool	isHot(const char* exchg, const char* rawCode, uint32_t dt = 0) override;
+public:
+    virtual const char* getRuleTag(const char* stdCode) override;
 
-	virtual bool	splitHotSecions(const char* exchg, const char* pid, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
+    virtual double getRuleFactor(const char* ruleTag, const char* fullPid, uint32_t uDate = 0) override;
 
-	//////////////////////////////////////////////////////////////////////////
-	//次主力接口
-	virtual const char* getSecondRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
+    //////////////////////////////////////////////////////////////////////////
+    // 主力接口
+    virtual const char* getRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
 
-	virtual const char* getPrevSecondRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
+    virtual const char* getPrevRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
 
-	virtual bool		isSecond(const char* exchg, const char* rawCode, uint32_t dt = 0) override;
+    virtual bool isHot(const char* exchg, const char* rawCode, uint32_t dt = 0) override;
 
-	virtual bool		splitSecondSecions(const char* exchg, const char* hotCode, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
+    virtual bool splitHotSecions(const char* exchg, const char* pid, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
 
-	//////////////////////////////////////////////////////////////////////////
-	//通用接口
-	virtual const char* getCustomRawCode(const char* tag, const char* fullPid, uint32_t dt) override;
+    //////////////////////////////////////////////////////////////////////////
+    // 次主力接口
+    virtual const char* getSecondRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
 
-	virtual const char* getPrevCustomRawCode(const char* tag, const char* fullPid, uint32_t dt) override;
+    virtual const char* getPrevSecondRawCode(const char* exchg, const char* pid, uint32_t dt = 0) override;
 
-	virtual bool		isCustomHot(const char* tag, const char* fullCode, uint32_t dt) override;
+    virtual bool isSecond(const char* exchg, const char* rawCode, uint32_t dt = 0) override;
 
-	virtual bool		splitCustomSections(const char* tag, const char* fullPid, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
+    virtual bool splitSecondSecions(const char* exchg, const char* hotCode, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
 
+    //////////////////////////////////////////////////////////////////////////
+    // 通用接口
+    virtual const char* getCustomRawCode(const char* tag, const char* fullPid, uint32_t dt) override;
+
+    virtual const char* getPrevCustomRawCode(const char* tag, const char* fullPid, uint32_t dt) override;
+
+    virtual bool isCustomHot(const char* tag, const char* fullCode, uint32_t dt) override;
+
+    virtual bool splitCustomSections(const char* tag, const char* fullPid, uint32_t sDt, uint32_t eDt, HotSections& sections) override;
 
 private:
-	//WTSExchgHotMap*	m_pExchgHotMap;
-	//WTSExchgHotMap*	m_pExchgScndMap;
-	//wt_hashset<std::string>	m_curHotCodes;
-	//wt_hashset<std::string>	m_curSecCodes;
-	bool			m_bInitialized;
+    // WTSExchgHotMap*	m_pExchgHotMap;
+    // WTSExchgHotMap*	m_pExchgScndMap;
+    // wt_hashset<std::string>	m_curHotCodes;
+    // wt_hashset<std::string>	m_curSecCodes;
+    bool m_bInitialized;
 
-	WTSCustomSwitchMap*	m_mapCustRules;
-	typedef wt_hashmap<std::string, wt_hashset<std::string>>	CustomSwitchCodes;
-	CustomSwitchCodes	m_mapCustCodes;
+    WTSCustomSwitchMap* m_mapCustRules;
+    typedef wt_hashmap<std::string, wt_hashset<std::string>> CustomSwitchCodes;
+    CustomSwitchCodes m_mapCustCodes;
 };
-
