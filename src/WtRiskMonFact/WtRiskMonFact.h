21b498a8ec10b8e98804bac61feef99e5921b187
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtRiskMonFact/WtRiskMonFact.h (clang-format: brace style, tabs->spaces)

--- src/WtRiskMonFact/WtRiskMonFact.h
+++ src/WtRiskMonFact/WtRiskMonFact.h
@@ -4,26 +4,23 @@
  * \author Wesley
  * \date 2020/03/30
  *
- * 
+ *
  */
 #pragma once
 #include "../Includes/RiskMonDefs.h"
 
 USING_NS_WTP;
 
-class WtRiskMonFact : public IRiskMonitorFact
-{
+class WtRiskMonFact : public IRiskMonitorFact {
 public:
-	WtRiskMonFact();
-	virtual ~WtRiskMonFact();
+    WtRiskMonFact();
+    virtual ~WtRiskMonFact();
 
 public:
-	virtual const char* getName() override;
-	virtual void enumRiskMonitors(FuncEnumRiskMonCallback cb) override;
+    virtual const char* getName() override;
+    virtual void enumRiskMonitors(FuncEnumRiskMonCallback cb) override;
 
-	virtual WtRiskMonitor* createRiskMonotor(const char* name) override;
+    virtual WtRiskMonitor* createRiskMonotor(const char* name) override;
 
-	virtual bool deleteRiskMonotor(WtRiskMonitor* unit) override;
-
+    virtual bool deleteRiskMonotor(WtRiskMonitor* unit) override;
 };
-
