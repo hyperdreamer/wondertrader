0962cf94d82429f6b6ce1c427120e87fe015d4bb
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:45 +0800

    [style] reformat Includes/IBtDtReader.h (clang-format: brace style, tabs->spaces)

--- src/Includes/IBtDtReader.h
+++ src/Includes/IBtDtReader.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <string>
@@ -19,13 +19,12 @@ class WTSVariant;
  *	@brief 数据读取模块回调接口
  *	@details 主要用于数据读取模块向调用模块回调
  */
-class IBtDtReaderSink
-{
+class IBtDtReaderSink {
 public:
-	/*
-	 *	@brief	输出数据读取模块的日志
-	 */
-	virtual void		reader_log(WTSLogLevel ll, const char* message) = 0;
+    /*
+     *	@brief	输出数据读取模块的日志
+     */
+    virtual void reader_log(WTSLogLevel ll, const char* message) = 0;
 };
 
 /*
@@ -33,29 +32,28 @@ public:
  *
  *	向核心模块提供行情数据(tick、K线)读取接口
  */
-class IBtDtReader
-{
+class IBtDtReader {
 public:
-	IBtDtReader() :_sink(NULL) {}
-	virtual ~IBtDtReader(){}
+    IBtDtReader(): _sink(NULL) {}
+    virtual ~IBtDtReader() {}
 
 public:
-	virtual void init(WTSVariant* cfg, IBtDtReaderSink* sink) { _sink = sink; }
+    virtual void init(WTSVariant* cfg, IBtDtReaderSink* sink) { _sink = sink; }
 
-	virtual bool read_raw_bars(const char* exchg, const char* code, WTSKlinePeriod period, std::string& buffer) = 0;
-	virtual bool read_raw_ticks(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) = 0;
+    virtual bool read_raw_bars(const char* exchg, const char* code, WTSKlinePeriod period, std::string& buffer) = 0;
+    virtual bool read_raw_ticks(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) = 0;
 
-	virtual bool read_raw_order_details(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
-	virtual bool read_raw_order_queues(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
-	virtual bool read_raw_transactions(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
+    virtual bool read_raw_order_details(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
+    virtual bool read_raw_order_queues(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
+    virtual bool read_raw_transactions(const char* exchg, const char* code, uint32_t uDate, std::string& buffer) { return false; }
 
 protected:
-	IBtDtReaderSink*	_sink;
+    IBtDtReaderSink* _sink;
 };
 
-//创建数据存储对象
+// 创建数据存储对象
 typedef IBtDtReader* (*FuncCreateBtDtReader)();
-//删除数据存储对象
-typedef void(*FuncDeleteBtDtReader)(IBtDtReader* store);
+// 删除数据存储对象
+typedef void (*FuncDeleteBtDtReader)(IBtDtReader* store);
 
 NS_WTP_END
