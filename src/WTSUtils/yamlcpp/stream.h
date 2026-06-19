e5b5bb41df2d9dea99b2e5c1914c9ca81fc1da43
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:51 +0800

    [style] reformat WTSUtils/yamlcpp/stream.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/stream.h
+++ src/WTSUtils/yamlcpp/stream.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -20,63 +20,71 @@ namespace YAML {
 class StreamCharSource;
 
 class Stream {
- public:
-  friend class StreamCharSource;
+public:
+    friend class StreamCharSource;
 
-  Stream(std::istream& input);
-  Stream(const Stream&) = delete;
-  Stream(Stream&&) = delete;
-  Stream& operator=(const Stream&) = delete;
-  Stream& operator=(Stream&&) = delete;
-  ~Stream();
+    Stream(std::istream& input);
+    Stream(const Stream&) = delete;
+    Stream(Stream&&) = delete;
+    Stream& operator=(const Stream&) = delete;
+    Stream& operator=(Stream&&) = delete;
+    ~Stream();
 
-  operator bool() const;
-  bool operator!() const { return !static_cast<bool>(*this); }
+    operator bool() const;
+    bool operator!() const { return !static_cast<bool>(*this); }
 
-  char peek() const;
-  char get();
-  std::string get(int n);
-  void eat(int n = 1);
+    char peek() const;
+    char get();
+    std::string get(int n);
+    void eat(int n = 1);
 
-  static char eof() { return 0x04; }
+    static char eof() { return 0x04; }
 
-  const Mark mark() const { return m_mark; }
-  int pos() const { return m_mark.pos; }
-  int line() const { return m_mark.line; }
-  int column() const { return m_mark.column; }
-  void ResetColumn() { m_mark.column = 0; }
+    const Mark mark() const { return m_mark; }
+    int pos() const { return m_mark.pos; }
+    int line() const { return m_mark.line; }
+    int column() const { return m_mark.column; }
+    void ResetColumn() { m_mark.column = 0; }
 
- private:
-  enum CharacterSet { utf8, utf16le, utf16be, utf32le, utf32be };
+private:
+    enum CharacterSet { utf8,
+                        utf16le,
+                        utf16be,
+                        utf32le,
+                        utf32be };
 
-  std::istream& m_input;
-  Mark m_mark;
+    std::istream& m_input;
+    Mark m_mark;
 
-  CharacterSet m_charSet;
-  mutable std::deque<char> m_readahead;
-  unsigned char* const m_pPrefetched;
-  mutable size_t m_nPrefetchedAvailable;
-  mutable size_t m_nPrefetchedUsed;
+    CharacterSet m_charSet;
+    mutable std::deque<char> m_readahead;
+    unsigned char* const m_pPrefetched;
+    mutable size_t m_nPrefetchedAvailable;
+    mutable size_t m_nPrefetchedUsed;
 
-  void AdvanceCurrent();
-  char CharAt(size_t i) const;
-  bool ReadAheadTo(size_t i) const;
-  bool _ReadAheadTo(size_t i) const;
-  void StreamInUtf8() const;
-  void StreamInUtf16() const;
-  void StreamInUtf32() const;
-  unsigned char GetNextByte() const;
+    void AdvanceCurrent();
+    char CharAt(size_t i) const;
+    bool ReadAheadTo(size_t i) const;
+    bool _ReadAheadTo(size_t i) const;
+    void StreamInUtf8() const;
+    void StreamInUtf16() const;
+    void StreamInUtf32() const;
+    unsigned char GetNextByte() const;
 };
 
 // CharAt
 // . Unchecked access
-inline char Stream::CharAt(size_t i) const { return m_readahead[i]; }
-
-inline bool Stream::ReadAheadTo(size_t i) const {
-  if (m_readahead.size() > i)
-    return true;
-  return _ReadAheadTo(i);
+inline char Stream::CharAt(size_t i) const
+{
+    return m_readahead[i];
 }
-}  // namespace YAML
 
-#endif  // STREAM_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+inline bool Stream::ReadAheadTo(size_t i) const
+{
+    if (m_readahead.size() > i)
+        return true;
+    return _ReadAheadTo(i);
+}
+} // namespace YAML
+
+#endif // STREAM_H_62B23520_7C8E_11DE_8A39_0800200C9A66
