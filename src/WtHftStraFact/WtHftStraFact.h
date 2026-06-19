904f60ed737d79fa46f9f2a5ec460159db7c7ce5
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtHftStraFact/WtHftStraFact.h (clang-format: brace style, tabs->spaces)

--- src/WtHftStraFact/WtHftStraFact.h
+++ src/WtHftStraFact/WtHftStraFact.h
@@ -3,19 +3,17 @@
 
 USING_NS_WTP;
 
-class WtHftStraFact : public IHftStrategyFact
-{
+class WtHftStraFact : public IHftStrategyFact {
 public:
-	WtHftStraFact();
-	virtual ~WtHftStraFact();
+    WtHftStraFact();
+    virtual ~WtHftStraFact();
 
 public:
-	virtual const char* getName() override;
+    virtual const char* getName() override;
 
-	virtual void enumStrategy(FuncEnumHftStrategyCallback cb) override;
+    virtual void enumStrategy(FuncEnumHftStrategyCallback cb) override;
 
-	virtual HftStrategy* createStrategy(const char* name, const char* id) override;
+    virtual HftStrategy* createStrategy(const char* name, const char* id) override;
 
-	virtual bool deleteStrategy(HftStrategy* stra) override;
+    virtual bool deleteStrategy(HftStrategy* stra) override;
 };
-
