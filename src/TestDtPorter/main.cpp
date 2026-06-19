21859ad7c97c39093f64d999dc74a53174c1e98c
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:47 +0800

    [style] reformat TestDtPorter/main.cpp (clang-format: brace style, tabs->spaces)

--- src/TestDtPorter/main.cpp
+++ src/TestDtPorter/main.cpp
@@ -6,23 +6,23 @@
 
 void on_get_bar(WTSBarStruct* bar, WtUInt32 count, bool isLast)
 {
-	fmt::print("{}.{}\n", bar->date, bar->time);
+    fmt::print("{}.{}\n", bar->date, bar->time);
 }
 
 void on_bar_cnt(WtUInt32 dataCnt)
 {
-	fmt::print("共{}条K线\n", dataCnt);
+    fmt::print("共{}条K线\n", dataCnt);
 }
 
 void on_log(const char* message)
 {
-	fmt::print(message);
-	fmt::print("\n");
+    fmt::print(message);
+    fmt::print("\n");
 }
 
 void testDtHelper()
 {
-	const char* session_str = "{\
+    const char* session_str = "{\
 		\"name\":\"股票白盘0930\",\
 		\"offset\" : 0,\
 		\"auction\" : {\
@@ -38,17 +38,17 @@ void testDtHelper()
 			}]\
 		}";
 
-	resample_bars("IC2009.dsb", on_get_bar, on_bar_cnt, 202001010931, 202009181500, "m1", 5, session_str, on_log);
+    resample_bars("IC2009.dsb", on_get_bar, on_bar_cnt, 202001010931, 202009181500, "m1", 5, session_str, on_log);
 }
 
 void test_porter()
 {
-	initialize("dtcfg.json", "logcfgdt.json", true, true);
-	start();
+    initialize("dtcfg.json", "logcfgdt.json", true, true);
+    start();
 }
 
 int main()
 {
-	test_porter();
+    test_porter();
     return 0;
 }
