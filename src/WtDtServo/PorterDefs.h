ea8b7102b69cae6fecdbcb263d32b50b52e9879a
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:03 +0800

    [style] reformat WtDtServo/PorterDefs.h (clang-format: brace style, tabs->spaces)

--- src/WtDtServo/PorterDefs.h
+++ src/WtDtServo/PorterDefs.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 
@@ -19,9 +19,8 @@ NS_WTP_END
 
 USING_NS_WTP;
 
-typedef void(PORTER_FLAG *FuncGetBarsCallback)(WTSBarStruct* bar, WtUInt32 count, bool isLast);
-typedef void(PORTER_FLAG *FuncGetTicksCallback)(WTSTickStruct* tick, WtUInt32 count, bool isLast);
-typedef void(PORTER_FLAG *FuncCountDataCallback)(WtUInt32 dataCnt);
-typedef void(PORTER_FLAG *FuncOnTickCallback)(const char* stdCode, WTSTickStruct* tick);
-typedef void(PORTER_FLAG *FuncOnBarCallback)(const char* stdCode, const char* period, WTSBarStruct* bar);
-
+typedef void(PORTER_FLAG* FuncGetBarsCallback)(WTSBarStruct* bar, WtUInt32 count, bool isLast);
+typedef void(PORTER_FLAG* FuncGetTicksCallback)(WTSTickStruct* tick, WtUInt32 count, bool isLast);
+typedef void(PORTER_FLAG* FuncCountDataCallback)(WtUInt32 dataCnt);
+typedef void(PORTER_FLAG* FuncOnTickCallback)(const char* stdCode, WTSTickStruct* tick);
+typedef void(PORTER_FLAG* FuncOnBarCallback)(const char* stdCode, const char* period, WTSBarStruct* bar);
