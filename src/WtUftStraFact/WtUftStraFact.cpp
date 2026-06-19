9e6a27f61a16777e3a1d4c17620ddbd6228e79c8
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftStraFact/WtUftStraFact.cpp (clang-format: brace style, tabs->spaces)

--- src/WtUftStraFact/WtUftStraFact.cpp
+++ src/WtUftStraFact/WtUftStraFact.cpp
@@ -5,26 +5,23 @@
 
 const char* FACT_NAME = "WtUftStraFact";
 
-extern "C"
+extern "C" {
+EXPORT_FLAG IUftStrategyFact* createStrategyFact()
 {
-	EXPORT_FLAG IUftStrategyFact* createStrategyFact()
-	{
-		IUftStrategyFact* fact = new WtUftStraFact();
-		return fact;
-	}
-
-	EXPORT_FLAG void deleteStrategyFact(IUftStrategyFact* fact)
-	{
-		if (fact != NULL)
-			delete fact;
-	}
+    IUftStrategyFact* fact = new WtUftStraFact();
+    return fact;
 }
 
+EXPORT_FLAG void deleteStrategyFact(IUftStrategyFact* fact)
+{
+    if (fact != NULL)
+        delete fact;
+}
+}
 
 WtUftStraFact::WtUftStraFact()
 {
 }
-
 
 WtUftStraFact::~WtUftStraFact()
 {
@@ -32,33 +29,31 @@ WtUftStraFact::~WtUftStraFact()
 
 const char* WtUftStraFact::getName()
 {
-	return FACT_NAME;
+    return FACT_NAME;
 }
 
 void WtUftStraFact::enumStrategy(FuncEnumUftStrategyCallback cb)
 {
-	cb(FACT_NAME, "SimpleUft", true);
+    cb(FACT_NAME, "SimpleUft", true);
 }
 
 UftStrategy* WtUftStraFact::createStrategy(const char* name, const char* id)
 {
-	if(strcmp(name, "SimpleUft") == 0)
-	{
-		return new WtUftStraDemo(id);
-	}
+    if (strcmp(name, "SimpleUft") == 0) {
+        return new WtUftStraDemo(id);
+    }
 
-
-	return NULL;
+    return NULL;
 }
 
 bool WtUftStraFact::deleteStrategy(UftStrategy* stra)
 {
-	if (stra == NULL)
-		return true;
+    if (stra == NULL)
+        return true;
 
-	if (strcmp(stra->getFactName(), FACT_NAME) != 0)
-		return false;
+    if (strcmp(stra->getFactName(), FACT_NAME) != 0)
+        return false;
 
-	delete stra;
-	return true;
+    delete stra;
+    return true;
 }
