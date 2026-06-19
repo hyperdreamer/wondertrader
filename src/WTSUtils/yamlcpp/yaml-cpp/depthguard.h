9b09ed93746106562d299c2587c802ba6ec1103d
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:51 +0800

    [style] reformat WTSUtils/yamlcpp/yaml-cpp/depthguard.h (clang-format: brace style, tabs->spaces)

--- src/WTSUtils/yamlcpp/yaml-cpp/depthguard.h
+++ src/WTSUtils/yamlcpp/yaml-cpp/depthguard.h
@@ -3,7 +3,7 @@
 
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
+     (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
@@ -21,17 +21,18 @@ namespace YAML {
  */
 class DeepRecursion : public ParserException {
 public:
-  virtual ~DeepRecursion() = default;
+    virtual ~DeepRecursion() = default;
 
-  DeepRecursion(int depth, const Mark& mark_, const std::string& msg_);
+    DeepRecursion(int depth, const Mark& mark_, const std::string& msg_);
 
-  // Returns the recursion depth when the exception was thrown
-  int depth() const {
-    return m_depth;
-  }
+    // Returns the recursion depth when the exception was thrown
+    int depth() const
+    {
+        return m_depth;
+    }
 
 private:
-  int m_depth = 0;
+    int m_depth = 0;
 };
 
 /**
@@ -45,31 +46,34 @@ private:
  * @param max_depth
  *  compile-time configurable maximum depth.
  */
-template <int max_depth = 2000>
+template<int max_depth = 2000>
 class DepthGuard final {
 public:
-  DepthGuard(int & depth_, const Mark& mark_, const std::string& msg_) : m_depth(depth_) {
-    ++m_depth;
-    if ( max_depth <= m_depth ) {
-        throw DeepRecursion{m_depth, mark_, msg_};
+    DepthGuard(int& depth_, const Mark& mark_, const std::string& msg_): m_depth(depth_)
+    {
+        ++m_depth;
+        if (max_depth <= m_depth) {
+            throw DeepRecursion{ m_depth, mark_, msg_ };
+        }
     }
-  }
 
-  DepthGuard(const DepthGuard & copy_ctor) = delete;
-  DepthGuard(DepthGuard && move_ctor) = delete;
-  DepthGuard & operator=(const DepthGuard & copy_assign) = delete;
-  DepthGuard & operator=(DepthGuard && move_assign) = delete;
+    DepthGuard(const DepthGuard& copy_ctor) = delete;
+    DepthGuard(DepthGuard&& move_ctor) = delete;
+    DepthGuard& operator=(const DepthGuard& copy_assign) = delete;
+    DepthGuard& operator=(DepthGuard&& move_assign) = delete;
 
-  ~DepthGuard() {
-    --m_depth;
-  }
+    ~DepthGuard()
+    {
+        --m_depth;
+    }
 
-  int current_depth() const {
-    return m_depth;
-  }
+    int current_depth() const
+    {
+        return m_depth;
+    }
 
 private:
-    int & m_depth;
+    int& m_depth;
 };
 
 } // namespace YAML
