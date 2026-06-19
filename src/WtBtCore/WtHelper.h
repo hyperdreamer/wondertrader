516158f3a74b47ad7b7ea494970427c03f1121f9
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:00 +0800

    [style] reformat WtBtCore/WtHelper.h (clang-format: brace style, tabs->spaces)

--- src/WtBtCore/WtHelper.h
+++ src/WtBtCore/WtHelper.h
@@ -4,26 +4,24 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <string>
 #include <stdint.h>
 
-class WtHelper
-{
+class WtHelper {
 public:
-	static std::string getCWD();
+    static std::string getCWD();
 
-	static const char* getOutputDir();
+    static const char* getOutputDir();
 
-	static const std::string& getInstDir() { return _inst_dir; }
-	static void setInstDir(const char* inst_dir) { _inst_dir = inst_dir; }
-	static void setOutputDir(const char* out_dir);
+    static const std::string& getInstDir() { return _inst_dir; }
+    static void setInstDir(const char* inst_dir) { _inst_dir = inst_dir; }
+    static void setOutputDir(const char* out_dir);
 
 private:
-	static std::string	_inst_dir;	//实例所在目录
-	static std::string	_out_dir;
+    static std::string _inst_dir; // 实例所在目录
+    static std::string _out_dir;
 };
-
