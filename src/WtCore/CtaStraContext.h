9fabd4a3d9bc89f137fea707fe339afabdc453a5
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:01 +0800

    [style] reformat WtCore/CtaStraContext.h (clang-format: brace style, tabs->spaces)

--- src/WtCore/CtaStraContext.h
+++ src/WtCore/CtaStraContext.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include "CtaStraBaseCtx.h"
@@ -20,27 +20,24 @@ USING_NS_WTP;
 
 class CtaStrategy;
 
-class CtaStraContext : public CtaStraBaseCtx
-{
+class CtaStraContext : public CtaStraBaseCtx {
 public:
-	CtaStraContext(WtCtaEngine* engine, const char* name, int32_t slippage);
-	virtual ~CtaStraContext();
+    CtaStraContext(WtCtaEngine* engine, const char* name, int32_t slippage);
+    virtual ~CtaStraContext();
 
-	void set_strategy(CtaStrategy* stra){ _strategy = stra; }
-	CtaStrategy* get_stragety() { return _strategy; }
+    void set_strategy(CtaStrategy* stra) { _strategy = stra; }
+    CtaStrategy* get_stragety() { return _strategy; }
 
 public:
-	//»Øµ÷º¯Êý
-	virtual void on_init() override;
-	virtual void on_session_begin(uint32_t uTDate) override;
-	virtual void on_session_end(uint32_t uTDate) override;
-	virtual void on_tick_updated(const char* stdCode, WTSTickData* newTick) override;
-	virtual void on_bar_close(const char* stdCode, const char* period, WTSBarStruct* newBar) override;
-	virtual void on_calculate(uint32_t curDate, uint32_t curTime) override;
-	virtual void on_condition_triggered(const char* stdCode, double target, double price, const char* usertag) override;
+    // »Øµ÷º¯Êý
+    virtual void on_init() override;
+    virtual void on_session_begin(uint32_t uTDate) override;
+    virtual void on_session_end(uint32_t uTDate) override;
+    virtual void on_tick_updated(const char* stdCode, WTSTickData* newTick) override;
+    virtual void on_bar_close(const char* stdCode, const char* period, WTSBarStruct* newBar) override;
+    virtual void on_calculate(uint32_t curDate, uint32_t curTime) override;
+    virtual void on_condition_triggered(const char* stdCode, double target, double price, const char* usertag) override;
 
 private:
-	CtaStrategy*		_strategy;
+    CtaStrategy* _strategy;
 };
-
-
