07ab56eb00f13b4cffa0b5913d2e9d007e2161ce
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:47 +0800

    [style] reformat ParserShm/ParserShm.h (clang-format: brace style, tabs->spaces)

--- src/ParserShm/ParserShm.h
+++ src/ParserShm/ParserShm.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include "../Includes/IParserApi.h"
@@ -19,75 +19,70 @@
 USING_NS_WTP;
 using namespace boost::asio;
 
-class ParserShm : public IParserApi
-{
+class ParserShm : public IParserApi {
 public:
-	ParserShm();
-	~ParserShm();
+    ParserShm();
+    ~ParserShm();
 
 #pragma pack(push, 8)
-	typedef struct _DataItem
-	{
-		uint32_t	_type;	//数据类型， 0-tick,1-委托队列,2-逐笔委托,3-逐笔成交
-		union
-		{
-			WTSTickStruct	_tick;
-			WTSOrdQueStruct _queue;
-			WTSOrdDtlStruct	_order;
-			WTSTransStruct	_trans;
-		};
+    typedef struct _DataItem {
+        uint32_t _type; // 数据类型， 0-tick,1-委托队列,2-逐笔委托,3-逐笔成交
+        union {
+            WTSTickStruct _tick;
+            WTSOrdQueStruct _queue;
+            WTSOrdDtlStruct _order;
+            WTSTransStruct _trans;
+        };
 
-		_DataItem() { memset(this, 0, sizeof(_DataItem)); }
-	} DataItem;
+        _DataItem() { memset(this, 0, sizeof(_DataItem)); }
+    } DataItem;
 
-	template <int N = 8 * 1024>
-	struct _DataQueue
-	{
-		uint64_t	_capacity = N;
-		volatile uint64_t	_readable;
-		volatile uint64_t	_writable;
-		uint32_t	_pid;
-		DataItem	_items[N];
+    template<int N = 8 * 1024>
+    struct _DataQueue {
+        uint64_t _capacity = N;
+        volatile uint64_t _readable;
+        volatile uint64_t _writable;
+        uint32_t _pid;
+        DataItem _items[N];
 
-		_DataQueue() :_readable(UINT64_MAX), _writable(0), _pid(0) {}
-	};
+        _DataQueue(): _readable(UINT64_MAX), _writable(0), _pid(0) {}
+    };
 
-	typedef _DataQueue<8 * 1024>	CastQueue;
+    typedef _DataQueue<8 * 1024> CastQueue;
 
 #pragma pack(pop)
 
 public:
-	virtual bool init(WTSVariant* config) override;
+    virtual bool init(WTSVariant* config) override;
 
-	virtual void release() override;
+    virtual void release() override;
 
-	virtual bool connect() override;
+    virtual bool connect() override;
 
-	virtual bool disconnect() override;
+    virtual bool disconnect() override;
 
-	virtual bool isConnected() override;
+    virtual bool isConnected() override;
 
-	virtual void subscribe(const CodeSet &vecSymbols) override;
-	virtual void unsubscribe(const CodeSet &vecSymbols) override;
+    virtual void subscribe(const CodeSet& vecSymbols) override;
+    virtual void unsubscribe(const CodeSet& vecSymbols) override;
 
-	virtual void registerSpi(IParserSpi* listener) override;
+    virtual void registerSpi(IParserSpi* listener) override;
 
 private:
-	std::string		_path;
-	typedef std::shared_ptr<BoostMappingFile> MappedFilePtr;
-	MappedFilePtr	_mapfile;
-	CastQueue*		_queue;
-	uint32_t		_gpsize;
-	uint32_t		_check_span;
-	uint32_t		_cpu;
+    std::string _path;
+    typedef std::shared_ptr<BoostMappingFile> MappedFilePtr;
+    MappedFilePtr _mapfile;
+    CastQueue* _queue;
+    uint32_t _gpsize;
+    uint32_t _check_span;
+    uint32_t _cpu;
 
-	IParserSpi*		_sink;
-	bool			_stopped;
+    IParserSpi* _sink;
+    bool _stopped;
 
-	std::vector<bool> _subbed;
+    std::vector<bool> _subbed;
 
-	IBaseDataMgr*	_bd_mgr;
+    IBaseDataMgr* _bd_mgr;
 
-	StdThreadPtr	_thrd_parser;
+    StdThreadPtr _thrd_parser;
 };
-
