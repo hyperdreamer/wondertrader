5911a729efdb9dd640fbf05ce282dcde487a78d6
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:49 +0800

    [style] reformat TraderYD/TraderYD.h (clang-format: brace style, tabs->spaces)

--- src/TraderYD/TraderYD.h
+++ src/TraderYD/TraderYD.h
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 
@@ -26,156 +26,144 @@
 
 USING_NS_WTP;
 
-class TraderYD : public ITraderApi, public YDListener
-{
+class TraderYD : public ITraderApi, public YDListener {
 public:
-	TraderYD();
-	virtual ~TraderYD();
-
-public:
-	virtual void notifyReadyForLogin(bool hasLoginFailed) override;
-
-
-	virtual void notifyLogin(int errorNo, int maxOrderRef, bool isMonitor) override;
-
-
-	virtual void notifyFinishInit() override;
-
-
-	virtual void notifyOrder(const YDOrder *pOrder, const YDInstrument *pInstrument, const YDAccount *pAccount) override;
-
-
-	virtual void notifyTrade(const YDTrade *pTrade, const YDInstrument *pInstrument, const YDAccount *pAccount) override;
-
-
-	virtual void notifyFailedOrder(const YDInputOrder *pFailedOrder, const YDInstrument *pInstrument, const YDAccount *pAccount) override;
-
-
-	virtual void notifyFailedCancelOrder(const YDFailedCancelOrder *pFailedCancelOrder, const YDExchange *pExchange, const YDAccount *pAccount) override;
-
-
-	virtual void notifyAccount(const YDAccount *pAccount) override;
-
-	virtual void notifyCaughtUp() override;
+    TraderYD();
+    virtual ~TraderYD();
 
 public:
-	typedef enum
-	{
-		WS_NOTLOGIN,		//未登录
-		WS_LOGINING,		//正在登录
-		WS_LOGINED,			//已登录
-		WS_LOGINFAILED,		//登录失败
-		WS_CONFIRM_QRYED,
-		WS_CONFIRMED,		//已确认
-		WS_ALLREADY			//全部就绪
-	} WrapperState;
+    virtual void notifyReadyForLogin(bool hasLoginFailed) override;
 
+    virtual void notifyLogin(int errorNo, int maxOrderRef, bool isMonitor) override;
+
+    virtual void notifyFinishInit() override;
+
+    virtual void notifyOrder(const YDOrder* pOrder, const YDInstrument* pInstrument, const YDAccount* pAccount) override;
+
+    virtual void notifyTrade(const YDTrade* pTrade, const YDInstrument* pInstrument, const YDAccount* pAccount) override;
+
+    virtual void notifyFailedOrder(const YDInputOrder* pFailedOrder, const YDInstrument* pInstrument, const YDAccount* pAccount) override;
+
+    virtual void notifyFailedCancelOrder(const YDFailedCancelOrder* pFailedCancelOrder, const YDExchange* pExchange, const YDAccount* pAccount) override;
+
+    virtual void notifyAccount(const YDAccount* pAccount) override;
+
+    virtual void notifyCaughtUp() override;
+
+public:
+    typedef enum {
+        WS_NOTLOGIN,    // 未登录
+        WS_LOGINING,    // 正在登录
+        WS_LOGINED,     // 已登录
+        WS_LOGINFAILED, // 登录失败
+        WS_CONFIRM_QRYED,
+        WS_CONFIRMED, // 已确认
+        WS_ALLREADY   // 全部就绪
+    } WrapperState;
 
 private:
-	int doLogin();
+    int doLogin();
 
-	//////////////////////////////////////////////////////////////////////////
-	//ITraderApi接口
+    //////////////////////////////////////////////////////////////////////////
+    // ITraderApi接口
 public:
-	virtual bool init(WTSVariant* params) override;
+    virtual bool init(WTSVariant* params) override;
 
-	virtual void release() override;
+    virtual void release() override;
 
-	virtual void registerSpi(ITraderSpi *listener) override;
+    virtual void registerSpi(ITraderSpi* listener) override;
 
-	virtual bool makeEntrustID(char* buffer, int length) override;
+    virtual bool makeEntrustID(char* buffer, int length) override;
 
-	virtual void connect() override;
+    virtual void connect() override;
 
-	virtual void disconnect() override;
+    virtual void disconnect() override;
 
-	virtual bool isConnected() override;
+    virtual bool isConnected() override;
 
-	virtual int login(const char* user, const char* pass, const char* productInfo) override;
+    virtual int login(const char* user, const char* pass, const char* productInfo) override;
 
-	virtual int logout() override;
+    virtual int logout() override;
 
-	virtual int orderInsert(WTSEntrust* eutrust) override;
+    virtual int orderInsert(WTSEntrust* eutrust) override;
 
-	virtual int orderAction(WTSEntrustAction* action) override;
+    virtual int orderAction(WTSEntrustAction* action) override;
 
-	virtual int queryAccount() override;
+    virtual int queryAccount() override;
 
-	virtual int queryPositions() override;
+    virtual int queryPositions() override;
 
-	virtual int queryOrders() override;
+    virtual int queryOrders() override;
 
-	virtual int queryTrades() override;
-
+    virtual int queryTrades() override;
 
 private:
-	 inline WTSOrderInfo*	makeOrderInfo(const YDOrder* orderField, const YDInstrument* instInfo) noexcept;
-	 inline WTSEntrust*		makeEntrust(const YDInputOrder *entrustField, const YDInstrument* instInfo) noexcept;
-	 inline WTSTradeInfo*	makeTradeRecord(const YDTrade *tradeField, const YDInstrument* instInfo) noexcept;
-	 inline WTSError*		makeError(int errorno, WTSErroCode ec) noexcept;
+    inline WTSOrderInfo* makeOrderInfo(const YDOrder* orderField, const YDInstrument* instInfo) noexcept;
+    inline WTSEntrust* makeEntrust(const YDInputOrder* entrustField, const YDInstrument* instInfo) noexcept;
+    inline WTSTradeInfo* makeTradeRecord(const YDTrade* tradeField, const YDInstrument* instInfo) noexcept;
+    inline WTSError* makeError(int errorno, WTSErroCode ec) noexcept;
 
-	 inline bool		generateEntrustID(uint32_t orderRef, char* buffer) noexcept;
-	 inline bool		extractEntrustID(const char* entrustid, uint32_t &orderRef) noexcept;
+    inline bool generateEntrustID(uint32_t orderRef, char* buffer) noexcept;
+    inline bool extractEntrustID(const char* entrustid, uint32_t& orderRef) noexcept;
 
-	inline uint32_t		genRequestID() noexcept
-	{
-		return m_iRequestID.fetch_add(1) + 1;
-	}
+    inline uint32_t genRequestID() noexcept
+    {
+        return m_iRequestID.fetch_add(1) + 1;
+    }
 
-	void on_kvcache_message(const char* message);
+    void on_kvcache_message(const char* message);
 
 protected:
-	std::string		m_strCfgFile;
+    std::string m_strCfgFile;
 
-	std::string		m_strUser;
-	std::string		m_strPass;
+    std::string m_strUser;
+    std::string m_strPass;
 
-	bool			m_bCatchup;
-	bool			m_bApiInited;
+    bool m_bCatchup;
+    bool m_bApiInited;
 
-	/*
-	 *	By Wesley @ 2023.12.27
-	 *	是否更新本地缓存，默认为false
-	 *	如果开启则会在运行中自动更新本地的成交、订单和持仓缓存
-	 */
-	bool			m_bUpdateCache;
+    /*
+     *	By Wesley @ 2023.12.27
+     *	是否更新本地缓存，默认为false
+     *	如果开启则会在运行中自动更新本地的成交、订单和持仓缓存
+     */
+    bool m_bUpdateCache;
 
-	std::string		m_strUserName;
+    std::string m_strUserName;
 
-	ITraderSpi*		m_sink;
-	uint64_t		m_uLastQryTime;
+    ITraderSpi* m_sink;
+    uint64_t m_uLastQryTime;
 
-	uint32_t					m_lDate;
-	std::atomic<uint32_t>		m_orderRef;		//报单引用
+    uint32_t m_lDate;
+    std::atomic<uint32_t> m_orderRef; // 报单引用
 
-	WrapperState				m_wrapperState;
+    WrapperState m_wrapperState;
 
-	YDApi*						m_pUserAPI;
-	std::atomic<uint32_t>		m_iRequestID;
+    YDApi* m_pUserAPI;
+    std::atomic<uint32_t> m_iRequestID;
 
-	typedef WTSHashMap<std::string> DataMap;
-	DataMap*					m_mapPosition;
-	DataMap*					m_mapTrades;
-	DataMap*					m_mapOrders;
-	WTSArray*					m_ayFunds;
+    typedef WTSHashMap<std::string> DataMap;
+    DataMap* m_mapPosition;
+    DataMap* m_mapTrades;
+    DataMap* m_mapOrders;
+    WTSArray* m_ayFunds;
 
-	IBaseDataMgr*				m_bdMgr;
+    IBaseDataMgr* m_bdMgr;
 
-	typedef std::queue<CommonExecuter>	QueryQue;
-	QueryQue				m_queQuery;
-	StdUniqueMutex			m_mtxQuery;
-	bool					m_bStopped;
-	StdThreadPtr			m_thrdWorker;
+    typedef std::queue<CommonExecuter> QueryQue;
+    QueryQue m_queQuery;
+    StdUniqueMutex m_mtxQuery;
+    bool m_bStopped;
+    StdThreadPtr m_thrdWorker;
 
-	std::string		m_strModule;
-	DllHandle		m_hInstYD;
-	typedef YDApi* (*YDCreator)(const char *);
-	YDCreator		m_funcCreator;
+    std::string m_strModule;
+    DllHandle m_hInstYD;
+    typedef YDApi* (*YDCreator)(const char*);
+    YDCreator m_funcCreator;
 
-	//委托单标记缓存器
-	WtKVCache		m_eidCache;
-	//订单标记缓存器
-	WtKVCache		m_oidCache;
-	CacheLogger		m_cacheLogger;
+    // 委托单标记缓存器
+    WtKVCache m_eidCache;
+    // 订单标记缓存器
+    WtKVCache m_oidCache;
+    CacheLogger m_cacheLogger;
 };
-
