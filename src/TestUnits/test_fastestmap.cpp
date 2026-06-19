4b54bf7380e08157f5c01d504ed3bca59a856062
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:48 +0800

    [style] reformat TestUnits/test_fastestmap.cpp (clang-format: brace style, tabs->spaces)

--- src/TestUnits/test_fastestmap.cpp
+++ src/TestUnits/test_fastestmap.cpp
@@ -13,87 +13,79 @@ USING_NS_WTP;
  */
 TEST(test_fastestmap, test_perform)
 {
-	fastest_hashmap<std::string, std::string> a;
-	wt_hashmap<std::string, std::string> b;
-	std::map<std::string, std::string> c;
-	std::unordered_map<std::string, std::string> d;
+    fastest_hashmap<std::string, std::string> a;
+    wt_hashmap<std::string, std::string> b;
+    std::map<std::string, std::string> c;
+    std::unordered_map<std::string, std::string> d;
 
-	uint32_t times = 300000;
+    uint32_t times = 300000;
 
-	char buffer[16] = { 0 };
-	TimeUtils::Ticker ticker;
+    char buffer[16] = { 0 };
+    TimeUtils::Ticker ticker;
 
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		a[buffer] = buffer;
-	}
-	uint64_t t1 = ticker.nano_seconds();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        a[buffer] = buffer;
+    }
+    uint64_t t1 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		b[buffer] = buffer;
-	}
-	uint64_t t2 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        b[buffer] = buffer;
+    }
+    uint64_t t2 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		c[buffer] = buffer;
-	}
-	uint64_t t3 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        c[buffer] = buffer;
+    }
+    uint64_t t3 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		d[buffer] = buffer;
-	}
-	uint64_t t4 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        d[buffer] = buffer;
+    }
+    uint64_t t4 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		a[buffer];
-	}
-	uint64_t t5 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        a[buffer];
+    }
+    uint64_t t5 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		b[buffer];
-	}
-	uint64_t t6 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        b[buffer];
+    }
+    uint64_t t6 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		c[buffer];
-	}
-	uint64_t t7 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        c[buffer];
+    }
+    uint64_t t7 = ticker.nano_seconds();
 
-	ticker.reset();
-	for (uint32_t i = 0; i < times; i++)
-	{
-		char* s = fmtutil::format_to(buffer, "{:09d}", i);
-		d[buffer];
-	}
-	uint64_t t8 = ticker.nano_seconds();
+    ticker.reset();
+    for (uint32_t i = 0; i < times; i++) {
+        char* s = fmtutil::format_to(buffer, "{:09d}", i);
+        d[buffer];
+    }
+    uint64_t t8 = ticker.nano_seconds();
 
-	fmt::print("write and read {} time with different maps\n", times);
-	fmt::print("robin_write: {}ns\n", t1);
-	fmt::print("ankerl_write: {}ns\n", t2);
-	fmt::print("stdmap_write: {}ns\n", t3);
-	fmt::print("stdunorder_write: {}ns\n", t4);
+    fmt::print("write and read {} time with different maps\n", times);
+    fmt::print("robin_write: {}ns\n", t1);
+    fmt::print("ankerl_write: {}ns\n", t2);
+    fmt::print("stdmap_write: {}ns\n", t3);
+    fmt::print("stdunorder_write: {}ns\n", t4);
 
-	fmt::print("robin_read: {}ns\n", t5);
-	fmt::print("ankerl_read: {}ns\n", t6);
-	fmt::print("stdmap_read: {}ns\n", t7);
-	fmt::print("stdunorder_read: {}ns\n", t8);
+    fmt::print("robin_read: {}ns\n", t5);
+    fmt::print("ankerl_read: {}ns\n", t6);
+    fmt::print("stdmap_read: {}ns\n", t7);
+    fmt::print("stdunorder_read: {}ns\n", t8);
 }
