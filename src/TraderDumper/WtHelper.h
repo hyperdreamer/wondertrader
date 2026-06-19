96fd6a117d110344bb0b81e5562b91a617347e86
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:10:49 +0800

    [style] reformat TraderDumper/WtHelper.h (clang-format: brace style, tabs->spaces)

--- src/TraderDumper/WtHelper.h
+++ src/TraderDumper/WtHelper.h
@@ -4,23 +4,21 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #pragma once
 #include <string>
 #include <stdint.h>
 
-class WtHelper
-{
+class WtHelper {
 public:
-	static const char* get_cwd();
+    static const char* get_cwd();
 
-	static const char* get_module_dir(){ return _bin_dir.c_str(); }
+    static const char* get_module_dir() { return _bin_dir.c_str(); }
 
-	static void set_module_dir(const char* mod_dir){ _bin_dir = mod_dir; }
+    static void set_module_dir(const char* mod_dir) { _bin_dir = mod_dir; }
 
 private:
-	static std::string	_bin_dir;
+    static std::string _bin_dir;
 };
-
