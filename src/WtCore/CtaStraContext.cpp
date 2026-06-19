a7a6cb84325f29975a7d0d33502e56937d60d8b1
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:00 +0800

    [style] reformat WtCore/CtaStraContext.cpp (clang-format: brace style, tabs->spaces)

--- src/WtCore/CtaStraContext.cpp
+++ src/WtCore/CtaStraContext.cpp
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #include "CtaStraContext.h"
 #include "WtCtaEngine.h"
@@ -15,70 +15,67 @@
 
 #include "../Includes/WTSContractInfo.hpp"
 
-
 CtaStraContext::CtaStraContext(WtCtaEngine* engine, const char* name, int32_t slippage)
-	: CtaStraBaseCtx(engine, name, slippage)
+    : CtaStraBaseCtx(engine, name, slippage)
 {
 }
-
 
 CtaStraContext::~CtaStraContext()
 {
 }
 
 //////////////////////////////////////////////////////////////////////////
-//»Øµ÷º¯Êý
+// »Øµ÷º¯Êý
 void CtaStraContext::on_bar_close(const char* code, const char* period, WTSBarStruct* newBar)
 {
-	if (_strategy)
-		_strategy->on_bar(this, code, period, newBar);
+    if (_strategy)
+        _strategy->on_bar(this, code, period, newBar);
 }
 
 void CtaStraContext::on_init()
 {
-	CtaStraBaseCtx::on_init();
+    CtaStraBaseCtx::on_init();
 
-	if (_strategy)
-		_strategy->on_init(this);
+    if (_strategy)
+        _strategy->on_init(this);
 
-	dump_chart_info();
+    dump_chart_info();
 }
 
 void CtaStraContext::on_session_begin(uint32_t uTDate)
 {
-	CtaStraBaseCtx::on_session_begin(uTDate);
+    CtaStraBaseCtx::on_session_begin(uTDate);
 
-	if (_strategy)
-		_strategy->on_session_begin(this, uTDate);
+    if (_strategy)
+        _strategy->on_session_begin(this, uTDate);
 }
 
 void CtaStraContext::on_session_end(uint32_t uTDate)
 {
-	if (_strategy)
-		_strategy->on_session_end(this, uTDate);
+    if (_strategy)
+        _strategy->on_session_end(this, uTDate);
 
-	CtaStraBaseCtx::on_session_end(uTDate);
+    CtaStraBaseCtx::on_session_end(uTDate);
 }
 
 void CtaStraContext::on_tick_updated(const char* code, WTSTickData* newTick)
 {
-	auto it = _tick_subs.find(code);
-	if (it == _tick_subs.end())
-		return;
+    auto it = _tick_subs.find(code);
+    if (it == _tick_subs.end())
+        return;
 
-	if (_strategy)
-		_strategy->on_tick(this, code, newTick);
+    if (_strategy)
+        _strategy->on_tick(this, code, newTick);
 }
 
 void CtaStraContext::on_calculate(uint32_t curDate, uint32_t curTime)
 {
-	if (_strategy)
-		_strategy->on_schedule(this, curDate, curTime);
+    if (_strategy)
+        _strategy->on_schedule(this, curDate, curTime);
 }
 
 void CtaStraContext::on_condition_triggered(const char* stdCode, double target, double price, const char* usertag)
 {
-	if (_strategy)
-		_strategy->on_condition_triggered(this, stdCode, target, price, usertag);
+    if (_strategy)
+        _strategy->on_condition_triggered(this, stdCode, target, price, usertag);
 }
-
