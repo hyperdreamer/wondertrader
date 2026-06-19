9e2865832920d285a76c1cad012f8745e1fa6a1b
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:04 +0800

    [style] reformat WtPorter/ExpExecuter.cpp (clang-format: brace style, tabs->spaces)

--- src/WtPorter/ExpExecuter.cpp
+++ src/WtPorter/ExpExecuter.cpp
@@ -5,18 +5,17 @@ extern WtRtRunner& getRunner();
 
 void ExpExecuter::init()
 {
-	getRunner().executer_init(name());
+    getRunner().executer_init(name());
 }
 
 void ExpExecuter::set_position(const wt_hashmap<std::string, double>& targets)
 {
-	for(auto& v : targets)
-	{
-		getRunner().executer_set_position(name(), v.first.c_str(), v.second);
-	}
+    for (auto& v : targets) {
+        getRunner().executer_set_position(name(), v.first.c_str(), v.second);
+    }
 }
 
 void ExpExecuter::on_position_changed(const char* stdCode, double targetPos)
 {
-	getRunner().executer_set_position(name(), stdCode, targetPos);
+    getRunner().executer_set_position(name(), stdCode, targetPos);
 }
