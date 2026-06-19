7feae754c6df4d565f0e079f355e5bd097cb3d5b
hyperdreamer <wenhepku@gmail.com>
2026-06-19 08:11:05 +0800

    [style] reformat WtUftCore/WtUftDtMgr.cpp (clang-format: brace style, tabs->spaces)

--- src/WtUftCore/WtUftDtMgr.cpp
+++ src/WtUftCore/WtUftDtMgr.cpp
@@ -4,8 +4,8 @@
  *
  * \author Wesley
  * \date 2020/03/30
- * 
- * \brief 
+ *
+ * \brief
  */
 #include "WtUftDtMgr.h"
 #include "WtUftEngine.h"
@@ -18,94 +18,87 @@
 #include "../WTSTools/WTSLogger.h"
 #include "../WTSTools/WTSDataFactory.h"
 
-
 WTSDataFactory g_dataFact;
 
 WtUftDtMgr::WtUftDtMgr()
-	: _engine(NULL)
-	, _bars_cache(NULL)
-	, _ticks_cache(NULL)
-	, _rt_tick_map(NULL)
+    : _engine(NULL), _bars_cache(NULL), _ticks_cache(NULL), _rt_tick_map(NULL)
 {
 }
 
-
 WtUftDtMgr::~WtUftDtMgr()
 {
-	if (_bars_cache)
-		_bars_cache->release();
+    if (_bars_cache)
+        _bars_cache->release();
 
-	if (_ticks_cache)
-		_ticks_cache->release();
+    if (_ticks_cache)
+        _ticks_cache->release();
 
-	if (_rt_tick_map)
-		_rt_tick_map->release();
+    if (_rt_tick_map)
+        _rt_tick_map->release();
 }
 
 bool WtUftDtMgr::init(WTSVariant* cfg, WtUftEngine* engine)
 {
-	_engine = engine;
+    _engine = engine;
 
-	return true;
+    return true;
 }
 
 void WtUftDtMgr::handle_push_quote(const char* stdCode, WTSTickData* newTick)
 {
-	if (newTick == NULL)
-		return;
+    if (newTick == NULL)
+        return;
 
-	if (_rt_tick_map == NULL)
-		_rt_tick_map = DataCacheMap::create();
+    if (_rt_tick_map == NULL)
+        _rt_tick_map = DataCacheMap::create();
 
-	_rt_tick_map->add(stdCode, newTick, true);
+    _rt_tick_map->add(stdCode, newTick, true);
 
-	if(_ticks_cache != NULL)
-	{
-		WTSHisTickData* tData = (WTSHisTickData*)_ticks_cache->get(stdCode);
-		if (tData == NULL)
-			return;
+    if (_ticks_cache != NULL) {
+        WTSHisTickData* tData = (WTSHisTickData*)_ticks_cache->get(stdCode);
+        if (tData == NULL)
+            return;
 
-		if (tData->isValidOnly() && newTick->volume() == 0)
-			return;
+        if (tData->isValidOnly() && newTick->volume() == 0)
+            return;
 
-		tData->appendTick(newTick->getTickStruct());
-	}
+        tData->appendTick(newTick->getTickStruct());
+    }
 }
 
 WTSTickData* WtUftDtMgr::grab_last_tick(const char* code)
 {
-	if (_rt_tick_map == NULL)
-		return NULL;
+    if (_rt_tick_map == NULL)
+        return NULL;
 
-	WTSTickData* curTick = (WTSTickData*)_rt_tick_map->grab(code);
-	if (curTick == NULL)
-		return NULL;
+    WTSTickData* curTick = (WTSTickData*)_rt_tick_map->grab(code);
+    if (curTick == NULL)
+        return NULL;
 
-	return curTick;
+    return curTick;
 }
-
 
 WTSTickSlice* WtUftDtMgr::get_tick_slice(const char* stdCode, uint32_t count, uint64_t etime /* = 0 */)
 {
-	return NULL;
+    return NULL;
 }
 
 WTSOrdQueSlice* WtUftDtMgr::get_order_queue_slice(const char* stdCode, uint32_t count, uint64_t etime /* = 0 */)
 {
-	return NULL;
+    return NULL;
 }
 
 WTSOrdDtlSlice* WtUftDtMgr::get_order_detail_slice(const char* stdCode, uint32_t count, uint64_t etime /* = 0 */)
 {
-	return NULL;
+    return NULL;
 }
 
 WTSTransSlice* WtUftDtMgr::get_transaction_slice(const char* stdCode, uint32_t count, uint64_t etime /* = 0 */)
 {
-	return NULL;
+    return NULL;
 }
 
 WTSKlineSlice* WtUftDtMgr::get_kline_slice(const char* stdCode, WTSKlinePeriod period, uint32_t times, uint32_t count, uint64_t etime /* = 0 */)
 {
-	return NULL;
+    return NULL;
 }
